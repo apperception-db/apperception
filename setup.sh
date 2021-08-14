6aced8f926992ae765cc1fa0ef4dd7deb42f912e
YongmingFrankGe <frankge_1012@outlook.com>
2021-08-13 22:09:27 -0700

    fix opencv version

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ mv ./yolov4-tiny.weights ./yolov4-deepsort/data
 mv ./yolov4.weights ./yolov4-deepsort/data
 pip3 install -r requirements.txt
 cd yolov4-deepsort
-pip3 install -r requirements.txt
 python3 save_model.py --model yolov4
 cd ..
 mv ./config.py ./yolov4-deepsort/core
