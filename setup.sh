283381f3d11ce3508f170648d51e23ad39efc04b
Yongming Ge <46760841+YongmingFrankGe@users.noreply.github.com>
2021-08-14 22:32:15 -0700

    update setup.sh with the model

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@ git clone https://github.com/theAIGuysCode/yolov4-deepsort.git
 mv ./yolov4-tiny.weights ./yolov4-deepsort/data
 mv ./yolov4.weights ./yolov4-deepsort/data
 pip3 install -r requirements.txt
-cd yolov4-deepsort
-python3 save_model.py --model yolov4
-cd ..
+mv checkpoint/ ./yolov4-deepsort
 mv ./config.py ./yolov4-deepsort/core
 
