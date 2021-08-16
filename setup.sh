1af172b0ec61901587949b0e3103b5f178e695ef
Vanessa Lin <valin@berkeley.edu>
2021-08-15 18:37:26 -0700

    changed line

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/theAIGuysCode/yolov4-deepsort.git
 mv ./yolov4-tiny.weights ./yolov4-deepsort/data
 mv ./yolov4.weights ./yolov4-deepsort/data
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 cd yolov4-deepsort
 python3 save_model.py --model yolov4
 cd ..
