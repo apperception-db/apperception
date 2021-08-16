8fcf240bce73c4189923f96d57250a1b4a655e20
Vanessa Lin <valin@berkeley.edu>
2021-08-15 18:37:26 -0700

    changed line

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ mv ./yolov4-tiny.weights ./yolov4-deepsort/data
 mv ./yolov4.weights ./yolov4-deepsort/data
 pip3 install -r requirements.txt
 mv checkpoint/ ./yolov4-deepsort
+pip install -r requirements.txt
+cd yolov4-deepsort
+python3 save_model.py --model yolov4
+cd ..
 mv ./config.py ./yolov4-deepsort/core
 mkdir output
