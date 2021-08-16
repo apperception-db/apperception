61caaebc65c2eb9ae9326af161389d6e8f1bc117
Yongming Ge <46760841+YongmingFrankGe@users.noreply.github.com>
2021-08-15 20:57:31 -0700

    Use pip3

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/theAIGuysCode/yolov4-deepsort.git
 mv ./yolov4-tiny.weights ./yolov4-deepsort/data
 mv ./yolov4.weights ./yolov4-deepsort/data
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 mv checkpoint/ ./yolov4-deepsort
 mv ./config.py ./yolov4-deepsort/core
 
