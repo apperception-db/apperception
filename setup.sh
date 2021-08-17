f413e9fc8ab214a7cc509d733aaf095a97500823
YongmingFrankGe <frankge_1012@outlook.com>
2021-08-16 21:29:43 -0700

    fix notebook output issue

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ git clone https://github.com/theAIGuysCode/yolov4-deepsort.git
 mv ./yolov4-tiny.weights ./yolov4-deepsort/data
 mv ./yolov4.weights ./yolov4-deepsort/data
 pip3 install -r requirements.txt
-mv checkpoint/ ./yolov4-deepsort
+mv checkpoints/ ./yolov4-deepsort
 mv ./config.py ./yolov4-deepsort/core
 mkdir output
