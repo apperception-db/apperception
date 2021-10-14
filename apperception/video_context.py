a03efef18b31a9983e3c7f8f1a1d6d58235193bd
Chanwut Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2021-10-14 15:30:15 -0700

    Add typings and move class/function comment inside or as docstring (#7)
    * add typings + move comments to docstring
    
    * fix error in typing
    
    * do not install type for mypy
    
    * fix type error
    
    * Automated lint and format fixes
    
    Co-authored-by: Github Actions Bot <apperception-actions-bot@users.noreply.github.com>

--- apperception/video_context.py
+++ apperception/video_context.py
@@ -1,133 +1,142 @@
-import ast
-import inspect
-import os
-from typing import Callable
-import uncompyle6
+from __future__ import annotations
+
+import datetime
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional
+
 import psycopg2
-from video_util import *
-import datetime 
+from lens import Lens
+from point import Point
+from tracker import Tracker
+from video_util import Units
 
 
-# Camera node
 class Camera:
-    def __init__(self, cam_id, point, ratio, video_file, metadata_id, lens):
-        self.cam_id = cam_id 
-        self.ratio = ratio
-        self.video_file = video_file
-        self.metadata_id = metadata_id
-        self.properties = []
+    """Camera node"""
+
+    def __init__(
+        self, cam_id: str, point: Point, ratio: float, video_file: str, metadata_id: str, lens: Lens
+    ):
+        self.cam_id: str = cam_id
+        self.ratio: float = ratio
+        self.video_file: str = video_file
+        self.metadata_id: str = metadata_id
+        self.properties: Dict[str, list] = {}  # TODO: what is the type of properties?
 
         # Contain objects that still have yet to be added to the backend
-        # If user calls recognize, those items will have already been 
-        # stored in the backend. These are reserved for objects that users 
+        # If user calls recognize, those items will have already been
+        # stored in the backend. These are reserved for objects that users
         # have not added to the camera.
-        self.items = [] 
-        self.object_recognition = None
-        self.point = point
-        self.lens = lens
+        self.items: List[Item] = []
+        self.object_recognition: Optional[ObjectRecognition] = None
+        self.point: Point = point
+        self.lens: Lens = lens
 
-    def add_item(self, item):
+    def add_item(self, item: Item):
         # Add item
         self.items.append(item)
 
-    def add_property(self, properties, property_type, new_prop):
+    def add_property(self, properties, property_type: str, new_prop):
+        # TODO: add type annotation
         # Add property
         self.properties[property_type].append(new_prop)
 
-    def add_lens(self, lens):
+    def add_lens(self, lens: Lens):
         # Add lens
         self.lens = lens
 
-    # Add a default add_recog_obj = True
-    def recognize(self, algo = 'Yolo', tracker_type = 'multi', tracker = None):
+    def recognize(
+        self, algo: str = "Yolo", tracker_type: str = "multi", tracker: Optional[Tracker] = None
+    ):
+        # Add a default add_recog_obj = True (TODO?)
         # Create object recognition node
         object_rec_node = ObjectRecognition(algo, tracker_type, tracker=None)
         self.object_recognition = object_rec_node
         return object_rec_node
 
-# Item node
+
+@dataclass
 class Item:
-    def __init__(self, item_id, item_type, location):
-        self.item_id = item_id
-        self.item_type = item_type
-        self.location = location
-        self.properties = {}
+    """Item node"""
+
+    item_id: str
+    item_type: str
+    location: Any  # TODO: what is the type of location?
+    properties: dict = field(default_factory=dict)  # TODO: what is the type of properties?
 
 
-# Object Recognition node
+@dataclass
 class ObjectRecognition:
-    def __init__(self, algo, tracker_type, tracker = None):
-        self.algo = algo
-        self.tracker_type = tracker_type
-        self.tracker = tracker
-        # bounding boxes from object recognition
-        self.bboxes = []
-        self.labels = None
-        self.tracked_cnt = None
+    """Object Recognition node"""
+
+    algo: str
+    tracker_type: str
+    tracker: Optional[Tracker] = None
+    bboxes: list = field(default_factory=list)  # TODO: what is the type of bboxes?
+    labels: Any = None  # TODO: what is the type of labels?
+    tracked_cnt: Any = None  # TODO: what is the type of trackd_cnt?
+    properties: Any = None  # TODO: what is the type of properties?
 
     def add_properties(self, properties):
         self.properties = properties
 
-class VideoContext:
-    def __init__(self, name, units):
-        self.root = self
-        self.name = name
-        self.units = units
-        self.camera_nodes = {}
-        self.start_time = datetime.datetime(2021, 6, 8, 7, 10, 28)
 
-    # Connect to the database
-    def connect_db(self, host='localhost', 
-                        user=None,
-                        password=None,
-                        port=5432,
-                        database_name=None):
-        self.conn = psycopg2.connect(database=database_name, user=user, 
-            password=password, host=host, port=port)
-        
+class VideoContext:
+    def __init__(self, name: str, units):
+        self.root: VideoContext = self
+        self.name: str = name
+        self.units: Units = units
+        self.camera_nodes: Dict[str, Camera] = {}
+        self.start_time: datetime.datetime = datetime.datetime(2021, 6, 8, 7, 10, 28)
+        self.conn: Any = None
+
+    def connect_db(self, host="localhost", user=None, password=None, port=5432, database_name=None):
+        """Connect to the database"""
+        self.conn = psycopg2.connect(
+            database=database_name, user=user, password=password, host=host, port=port
+        )
+
     def get_name(self):
         return self.name
-    
+
     def get_units(self):
         return self.units
 
-    # Establish camera
-    def camera(self, cam_id, point, ratio, video_file, metadata_id, lens):
+    def camera(
+        self, cam_id: str, point: Point, ratio: float, video_file: str, metadata_id: str, lens: Lens
+    ):
+        """Establish camera"""
         camera_node = self.__get_camera(cam_id)
         if not camera_node:
             camera_node = Camera(cam_id, point, ratio, video_file, metadata_id, lens)
             self.__add_camera(cam_id, camera_node)
         return camera_node
 
-    def properties(self, cam_id, properties, property_type):
+    def properties(self, cam_id: str, properties, property_type):
         camera_node = self.__get_camera(cam_id)
         if not camera_node:
             return None
-     
-        camera_node.add_properties(properties, property_type)
-       # Display error 
 
-    def get_camera(self, cam_id):
+        camera_node.add_properties(properties, property_type)
+        # Display error
+
+    def get_camera(self, cam_id: str):
         return self.__get_camera(cam_id)
-    
-    # Get camera
-    def __get_camera(self, cam_id):
+
+    def __get_camera(self, cam_id: str):
+        """Get camera"""
         if cam_id in self.camera_nodes.keys():
-          return self.camera_nodes[cam_id]
+            return self.camera_nodes[cam_id]
         return None
 
-    # Add camera
-    def __add_camera(self, cam_id, camera_node):
+    def __add_camera(self, cam_id: str, camera_node: Camera):
+        """Add camera"""
         self.camera_nodes[cam_id] = camera_node
 
-    # Remove camera
-    def remove_camera(self, cam_id):
-        camera_node = self.__get_camera(cam_id)
-        self.camera_nodes.remove(camera_node)
+    def remove_camera(self, cam_id: str):
+        """Remove camera"""
+        del self.camera_nodes[cam_id]
 
-    # Clear
     def clear(self):
-        self.camera_nodes = []
-
-    
-
+        """Clear"""
+        self.camera_nodes = {}
--- apperception/video_context_executor.py
+++ apperception/video_context_executor.py
@@ -1,17 +1,22 @@
-from video_context import VideoContext
-from video_util import create_or_insert_world_table, create_or_insert_camera_table, recognize, video_data_to_tasm, add_recognized_objs, metadata_to_tasm
+from typing import Dict, List
 
-import json
+from video_context import Camera, ObjectRecognition, VideoContext
+from video_util import (add_recognized_objs, create_or_insert_camera_table,
+                        create_or_insert_world_table, metadata_to_tasm,
+                        recognize, video_data_to_tasm)
 
-# TODO: Add checks for Nones 
+recognized_camera: Dict[str, bool] = {}
+
+
 class VideoContextExecutor:
-    def __init__(self, conn, new_video_context:VideoContext=None, tasm=None):
+    # TODO: Add checks for Nones
+    def __init__(self, conn, new_video_context: VideoContext = None, tasm=None):
         if new_video_context:
             self.context(new_video_context)
         self.conn = conn
         self.tasm = tasm
 
-    def context(self, video_context:VideoContext):
+    def context(self, video_context: VideoContext):
         self.current_context = video_context
         return self
 
@@ -20,19 +25,22 @@ class VideoContextExecutor:
         return video_query
 
     def visit_world(self):
-        # Query to store world in database 
+        # Query to store world in database
         name, units = self.current_context.name, self.current_context.units
         world_sql = create_or_insert_world_table(self.conn, name, units)
 
-        all_sqls = []
+        all_sqls: List[str] = []
         cameras = self.current_context.camera_nodes
         if len(cameras) != 0:
             for c in cameras.values():
-                camera_sql = self.visit_camera(c)
-                all_sqls.append(camera_sql)
+                cam_id = c.cam_id
+                if not recognized_camera.get(cam_id, False):
+                    camera_sql = self.visit_camera(c)
+                    all_sqls.append(camera_sql)
+                    recognized_camera[cam_id] = True
         return all_sqls
 
-    def visit_camera(self, camera_node):
+    def visit_camera(self, camera_node: Camera):
         world_name = self.current_context.name
         camera_sql = create_or_insert_camera_table(self.conn, world_name, camera_node)
         if camera_node.object_recognition is not None:
@@ -41,8 +49,7 @@ class VideoContextExecutor:
             video_data_to_tasm(camera_node.video_file, camera_node.metadata_id, self.tasm)
         return camera_sql
 
-    def visit_obj_rec(self, camera_node, object_rec_node):
-        cam_id = camera_node.cam_id
+    def visit_obj_rec(self, camera_node: Camera, object_rec_node: ObjectRecognition):
         lens = camera_node.lens
         video_file = camera_node.video_file
 
@@ -52,12 +59,10 @@ class VideoContextExecutor:
         tracker_type = object_rec_node.tracker_type
         algo = object_rec_node.algo
 
-        
         tracking_results = recognize(video_file, algo, tracker_type, tracker)
         add_recognized_objs(self.conn, lens, tracking_results, start_time)
         if self.tasm:
             metadata_to_tasm(tracking_results, camera_node.metadata_id, self.tasm)
-        
+
     def execute(self):
         query = self.visit()
-
