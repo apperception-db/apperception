4df9311edb672d539c2953453ac587ebf4b95669
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2021-10-25 12:51:14 -0700

    remove printing/saving options from yolov5-deepsort

--- apperception/object_tracker_2.py
+++ apperception/object_tracker_2.py
@@ -7,19 +7,12 @@ sys.path.append(os.path.join(CURRENT_DIR, "../yolov5-deepsort/"))
 
 from yolov5.models.experimental import attempt_load
 from yolov5.utils.downloads import attempt_download
-from yolov5.utils.datasets import LoadImages, LoadStreams
-from yolov5.utils.general import check_img_size, non_max_suppression, scale_coords, check_imshow, xyxy2xywh
-from yolov5.utils.torch_utils import select_device  # , time_sync
-from yolov5.utils.plots import Annotator, colors
+from yolov5.utils.datasets import LoadImages
+from yolov5.utils.general import check_img_size, non_max_suppression, scale_coords, xyxy2xywh
+from yolov5.utils.torch_utils import select_device
 from deep_sort_pytorch.utils.parser import get_config
 from deep_sort_pytorch.deep_sort import DeepSort
-# import platform
-import shutil
-# import time
-from pathlib import Path
-import cv2
 import torch
-import torch.backends.cudnn as cudnn
 from typing import Dict, List, Tuple, Optional, TypedDict, Union
 from dataclasses import dataclass
 
@@ -52,11 +45,9 @@ class FormattedResult(TypedDict):
 
 
 def detect(opt: YoloV5Opt):
-    out, source, yolo_weights, deep_sort_weights, show_vid, save_vid, save_txt, imgsz, evaluate = \
-        opt.output, opt.source, opt.yolo_weights, opt.deep_sort_weights, opt.show_vid, opt.save_vid, \
-        opt.save_txt, opt.img_size, opt.evaluate
-    webcam = source == '0' or source.startswith(
-        'rtsp') or source.startswith('http') or source.endswith('.txt')
+    source, yolo_weights, deep_sort_weights, imgsz = \
+        opt.source, opt.yolo_weights, opt.deep_sort_weights, \
+        opt.img_size
 
     # initialize deepsort
     cfg = get_config()
@@ -71,14 +62,6 @@ def detect(opt: YoloV5Opt):
     # Initialize
     device = select_device(opt.device)
 
-    # The MOT16 evaluation runs multiple inference streams in parallel, each one writing to
-    # its own .txt file. Hence, in that case, the output folder is not restored
-    if not evaluate:
-        if os.path.exists(out):
-            pass
-            shutil.rmtree(out)  # delete output folder
-        os.makedirs(out)  # make new output folder
-
     half = device.type != 'cpu'  # half precision only supported on CUDA
     # Load model
     model = attempt_load(yolo_weights, map_location=device)  # load FP32 model
@@ -88,17 +71,7 @@ def detect(opt: YoloV5Opt):
     if half:
         model.half()  # to FP16
 
-    # Set Dataloader
-    vid_path, vid_writer = None, None
-    # Check if environment supports image displays
-    if show_vid:
-        show_vid = check_imshow()
-
-    if webcam:
-        cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride)
-    else:
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+    dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
     # Get names and colors
     names = model.module.names if hasattr(model, 'module') else model.names
@@ -107,16 +80,9 @@ def detect(opt: YoloV5Opt):
     if device.type != 'cpu':
         imgsz1, imgsz2 = (imgsz, imgsz) if isinstance(imgsz, int) else imgsz
         model(torch.zeros(1, 3, imgsz1, imgsz2).to(device).type_as(next(model.parameters())))  # run once
-    # t0 = time.time()
-
-    save_path = str(Path(out))
-    # extract what is in between the last '/' and last '.'
-    txt_file_name = source.split('/')[-1].split('.')[0]
-    txt_path = str(Path(out)) + '/' + txt_file_name + '.txt'
 
     formatted_result: Dict[str, FormattedResult] = {}
-
-    for frame_idx, (path, img, im0s, vid_cap) in enumerate(dataset):
+    for frame_idx, (_, img, im0s, _) in enumerate(dataset):
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -124,116 +90,47 @@ def detect(opt: YoloV5Opt):
             img = img.unsqueeze(0)
 
         # Inference
-        # t1 = time_sync()
         pred = model(img, augment=opt.augment)[0]
 
         # Apply NMS
         pred = non_max_suppression(
             pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
-        # t2 = time_sync()
 
         # Process detections
-        for i, det in enumerate(pred):  # detections per image
-            if webcam:  # batch_size >= 1
-                p, s, im0 = path[i], '%g: ' % i, im0s[i].copy()
-            else:
-                p, s, im0 = path, '', im0s
+        for det in pred:  # detections per image
 
-            s += '%gx%g ' % img.shape[2:]  # print string
-            save_path = str(Path(out) / Path(p).name)
-
-            annotator = Annotator(im0, line_width=2, pil=not ascii)
-
-            if det is not None and len(det):
-                # Rescale boxes from img_size to im0 size
-                det[:, :4] = scale_coords(
-                    img.shape[2:], det[:, :4], im0.shape).round()
-
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
-                xywhs = xyxy2xywh(det[:, 0:4])
-                confs = det[:, 4]
-                clss = det[:, 5]
-
-                # pass detections to deepsort
-                outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0)
-
-                # draw boxes for visualization
-                if len(outputs) > 0:
-                    for j, (output, conf) in enumerate(zip(outputs, confs)):
-
-                        bboxes = output[0:4]
-                        id = output[4]
-                        cls = output[5]
-
-                        c = int(cls)  # integer class
-                        label = f'{id} {names[c]} {conf:.2f}'
-                        annotator.box_label(bboxes, label, color=colors(c, True))
-
-                        item_id = f"{names[c]}-{str(id)}"
-                        if item_id in formatted_result:
-                            formatted_result[item_id]["bboxes"].append(
-                                [[int(bboxes[0]), int(bboxes[1])], [int(bboxes[2]), int(bboxes[3])]]
-                            )
-                            formatted_result[item_id]["tracked_cnt"].append(frame_idx)
-                        else:
-                            formatted_result[item_id] = {
-                                "object_type": names[c],
-                                "bboxes": [[[int(bboxes[0]), int(bboxes[1])], [int(bboxes[2]), int(bboxes[3])]]],
-                                "tracked_cnt": [frame_idx],
-                            }
-
-                        if save_txt:
-                            # to MOT format
-                            bbox_left = output[0]
-                            bbox_top = output[1]
-                            bbox_w = output[2] - output[0]
-                            bbox_h = output[3] - output[1]
-                            # Write MOT compliant results to file
-                            with open(txt_path, 'a') as f:
-                                f.write(('%g ' * 10 + '\n') % (frame_idx, id, bbox_left, bbox_top, bbox_w, bbox_h, -1, -1, -1, -1))  # label format
-
-            else:
+            if det is None or not len(det):
                 deepsort.increment_ages()
+                continue
 
-            # Print time (inference + NMS)
-            # print('%sDone. (%.3fs)' % (s, t2 - t1))
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(
+                img.shape[2:], det[:, :4], im0s.shape).round()
 
-            # Stream results
-            im0 = annotator.result()
-            if show_vid:
-                cv2.imshow(p, im0)
-                if cv2.waitKey(1) == ord('q'):  # q to quit
-                    raise StopIteration
+            xywhs = xyxy2xywh(det[:, 0:4])
+            confs = det[:, 4]
+            clss = det[:, 5]
 
-            # Save results (image with detections)
-            if save_vid:
-                if vid_path != save_path:  # new video
-                    vid_path = save_path
-                    if isinstance(vid_writer, cv2.VideoWriter):
-                        vid_writer.release()  # release previous video writer
-                    if vid_cap:  # video
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                    else:  # stream
-                        fps, w, h = 30, im0.shape[1], im0.shape[0]
-                        save_path += '.mp4'
+            # pass detections to deepsort
+            outputs = deepsort.update(xywhs.cpu(), confs.cpu(), clss.cpu(), im0s)
 
-                    vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+            # collect result bounding boxes
+            for output in outputs:
 
-                if vid_writer:
-                    vid_writer.write(im0)
+                x1, y1, x2, y2, id, c = [int(o) for o in output]
+                bboxes = [[x1, y1], [x2, y2]]
+                item_id = f"{names[c]}-{str(id)}"
 
-    # if save_txt or save_vid:
-    #     print('Results saved to %s' % os.getcwd() + os.sep + out)
-    #     if platform == 'darwin':  # MacOS
-    #         os.system('open ' + save_path)
+                if item_id in formatted_result:
+                    formatted_result[item_id]["bboxes"].append(bboxes)
+                    formatted_result[item_id]["tracked_cnt"].append(frame_idx)
+                else:
+                    formatted_result[item_id] = {
+                        "object_type": names[c],
+                        "bboxes": [bboxes],
+                        "tracked_cnt": [frame_idx],
+                    }
 
-    # print('Done. (%.3fs)' % (time.time() - t0))
     return formatted_result
 
 
