0782158731fd983ec92338c1d6b4998a2f270ef2
YongmingFrankGe <frankge_1012@outlook.com>
2021-10-31 14:27:46 -0700

    similarity join storage implemented

--- apperception/apperception_benchmark.py
+++ apperception/apperception_benchmark.py
@@ -37,25 +37,36 @@ traffic_world = traffic_world.camera(cam_id=cam1_id,
                                video_file=cam1_video_file, 
                                metadata_identifier=name+"_"+cam1_id, 
                                lens=cam1_len)
+traffic_world = traffic_world.camera(cam_id=cam2_id, 
+                               video_file=cam2_video_file, 
+                               metadata_identifier=name+"_"+cam2_id, 
+                               lens=cam2_len)
 
 ### Call execute on the world to run the detection algorithm and save the real data to the database
-recognized_world = traffic_world.recognize(cam1_id)
-recognized_world.execute()
+# recognized_world = traffic_world.recognize(cam1_id)
+# recognized_world.execute()
 
-# volume = traffic_world.select_intersection_of_interest_or_use_default(cam_id=cam_id)
-# filtered_world = traffic_world.predicate(lambda obj:obj.object_type == "car").predicate(lambda obj:obj.location in volume, {"volume":volume})
+# tracking_results = recognize(cam2_video_file)
+# file1 = open("resultfile_2.txt","w")
+# file1.write(str(tracking_results))
+# file1.close()
+# conn = psycopg2.connect(database="mobilitydb", user="docker", password="docker", host="localhost", port=25432)
+# add_recognized_objs(conn, cam2_len, tracking_results, traffic_world.VideoContext.start_time, temp=True)
+
+volume = traffic_world.select_intersection_of_interest_or_use_default(cam_id=cam2_id)
+filtered_world = traffic_world.predicate(lambda obj:obj.object_type == "car")
 # filtered_world = filtered_world.interval([0,fps*3])
  
-# ### to get the trajectory and the video over the entire trajectory(amber case)
-# filtered_ids = filtered_world.selectkey(distinct = True).execute()
-# print("filtered_ids are", filtered_ids)
-# print(len(filtered_ids))
-# if len(filtered_ids)>0:
-#     id_array = [e[0] for e in filtered_ids]
-#     ### Fetch the trajectory of these items
-#     trajectory = traffic_world.predicate(lambda obj: obj.object_id in id_array, {"id_array":id_array}).get_trajectory(distinct=True).execute()
-#     traffic_world.overlay_trajectory(cam_id, trajectory)
-    ### Get the videos of these items
-#     entire_video = traffic_world.predicate(lambda obj: obj.object_id in id_array, {"id_array":id_array}).get_video()
-#     entire_video.execute()
+### to get the trajectory and the video over the entire trajectory(amber case)
+filtered_ids = filtered_world.selectkey(distinct = True).execute()
+print("filtered_ids are", filtered_ids)
+print(len(filtered_ids))
+if len(filtered_ids)>0:
+    id_array = [e[0] for e in filtered_ids]
+    ### Fetch the trajectory of these items
+    trajectory = traffic_world.predicate(lambda obj: obj.object_id in id_array, {"id_array":id_array}).get_trajectory(distinct=True).execute()
+    traffic_world.overlay_trajectory(cam2_id, trajectory)
+    ## Get the videos of these items
+    entire_video = traffic_world.predicate(lambda obj: obj.object_id in id_array, {"id_array":id_array}).get_video()
+    entire_video.execute()
 
