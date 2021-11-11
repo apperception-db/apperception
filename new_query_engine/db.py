f5a2bb2d29b15280d48f959bf5d116fb21f21e4a
Byron <byronhsu1230@gmail.com>
2021-11-11 12:20:21 -0800

    add readme

--- new_query_engine/db.py
+++ new_query_engine/db.py
@@ -4,32 +4,45 @@ from pypika import Query, Table, Field
 
 class Database:
     def __init__(self):
+        # should setup a postgres in docker first
         self.con = psycopg2.connect(dbname = "postgres", user = "postgres", host = "localhost", port = "5432", password = "1234")
         self.cur = self.con.cursor()
 
-        # use pypika to create table
+        # create camera table
         self.cur.execute("DROP TABLE IF EXISTS cam")
         self.cur.execute("CREATE TABLE cam (cam_id text, size int, world_id text)")
         self.con.commit()
 
-    def insert_cam(self, cam_id, cam_size, world_id):
+    def insert_cam(self, cam_id: str, cam_size: int, world_id: str):
         cam = Table("cam")
         q = Query.into(cam).insert(cam_id, cam_size, world_id)
         self.cur.execute(q.get_sql())
         self.con.commit()
 
-    def concat_with(self, query, world_id):
+    def concat_with(self, query: Query, world_id: str):
+        """
+        Called when executing update commands (add_camera, add_objs ...etc)
+        """
         return query + self._select_cam_with_world_id(world_id) if query \
                 else self._select_cam_with_world_id(world_id) # UNION
 
-    def nest_from(self, sub_query, condition):
+    def nest_from(self, sub_query: Query, condition: str):
+        """
+        Called when executing filter commands (predicate, interval ...etc) 
+        """
         return Query.from_(sub_query).select("*").where(eval(condition))
 
-    def execute_get_query(self, query):
+    def execute_get_query(self, query: Query):
+        """
+        Execute sql command rapidly
+        """
         self.cur.execute(query.get_sql())
         return self.cur.fetchall()
 
-    def _select_cam_with_world_id(self, world_id):
+    def _select_cam_with_world_id(self, world_id: str):
+        """
+        Select cams with certain world id
+        """
         cam = Table("cam")
         q = Query.from_(cam).select("*").where(cam.world_id == world_id)
         return q
--- new_query_engine/readme.md
+++ new_query_engine/readme.md
@@ -1,5 +1,24 @@
-## New query engine
+# New query engine
 
-### Prerequisite
-- postgresql
+## Prerequisite
+- Create a docker container for postgres
+
+```
+docker volume create --name postgresql-data
+docker run -d -p 5432:5432 --name mypostgres --restart always -v postgresql-data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=1234 postgres
+```
+
+## Design
+
+### Data Persistency
+### Query Optimization
+
+- Each world will have access to the list of operations done since the first world.
+- Each item in the DB will have a row indicating the world id that creates it.
+- When call operations on a world, we do not execute them right away. Instead, we collect these operations to be a sequence of operations.
+- When we call get-data method (such as selectKey / get_trajectories) we construct an SQL - query based on the sequence of operations.
+- When see predicates, we nest the query of the old world. 
+- When see update, we concat the query of the old world.
+
+
 
