8d11cc5f71c006cfd8016cdb5656d7b0a66f0d07
Byron <byronhsu1230@gmail.com>
2022-02-25 11:06:46 -0800

    add compute lib

---
+++ apperception/new_compute_lib.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def compute_heading(trajectories):
+    headings = []
+    for traj in trajectories:
+        traj = traj[0]
+        heading = [None]
+        for j in range(1, len(traj)):
+            prev_pos = traj[j - 1]
+            current_pos = traj[j]
+            heading.append(0)
+            if current_pos[1] != prev_pos[1]:
+                heading[j] = np.arctan2(
+                    current_pos[1] - prev_pos[1], current_pos[0] - prev_pos[0]
+                )
+            heading[j] *= 180 / np.pi  # convert to degrees from radian
+            heading[j] = (
+                heading[j] + 360
+            ) % 360  # converting such that all headings are positive
+        headings.append(heading)
+    return headings
