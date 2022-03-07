404b80ff8b5555c3fb77dfcb707a11cc127a4361
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-03-07 03:50:23 -0800

    set up nbstripout

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ mkdir output
 # setup python environments and dependencies
 python3 -m venv env
 source env/bin/activate
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+python3 -m pip install -q --upgrade pip
+python3 -m pip install -q -r requirements.txt
+nbstripout --install
 
 # setup YoloV4
 git clone https://github.com/theAIGuysCode/yolov4-deepsort.git
@@ -19,5 +20,5 @@ cp ./configs/yolov4-config.py ./yolov4-deepsort/core/config.py
 git clone --recurse-submodules git@github.com:mikel-brostrom/Yolov5_DeepSort_Pytorch.git yolov5-deepsort
 cp ./configs/yolov5-deepsort-config.yaml ./yolov5-deepsort/deep_sort_pytorch/configs/deep_sort.yaml
 pushd yolov5-deepsort
-python3 -m pip install -r requirements.txt
+python3 -m pip install -q -r requirements.txt
 popd
