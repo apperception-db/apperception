4bb75028ff4c53d3056110b5ec38f0f0916d1d02
yousefh409 <yousefh409@gmail.com>
2022-04-21 12:53:03 -0700

    fixed bug with roadDirection and facingRelative

--- pg_extender/facingRelative.sql
+++ pg_extender/facingRelative.sql
@@ -1,5 +1,5 @@
-DROP FUNCTION IF EXISTS facingRelative(real, real, timestamptz);
-CREATE OR REPLACE FUNCTION facingRelative(object_heading real, camera_heading real, _time timestamptz) RETURNS real AS
+DROP FUNCTION IF EXISTS facingRelative(real, real);
+CREATE OR REPLACE FUNCTION facingRelative(object_heading real, camera_heading real) RETURNS real AS
 $BODY$
 BEGIN
   RETURN object_heading - camera_heading;
@@ -12,8 +12,7 @@ DROP FUNCTION IF EXISTS facingRelative(tfloat, real, timestamptz);
 CREATE OR REPLACE FUNCTION facingRelative(object_headings tfloat, camera_heading real, _time timestamptz) RETURNS real AS
 $BODY$
 BEGIN
-  -- TODO: get object heading at timestampz
-  RETURN facingRelative(valueAtTimestamp(object_headings, _time), camera_heading, _time);
+  RETURN facingRelative(CAST(valueAtTimestamp(object_headings, _time) AS real), camera_heading);
 END
 $BODY$
 LANGUAGE 'plpgsql' ;
--- pg_extender/roadDirection.sql
+++ pg_extender/roadDirection.sql
@@ -2,7 +2,7 @@ DROP FUNCTION IF EXISTS roadDirection(real, real);
 CREATE OR REPLACE FUNCTION roadDirection(x real, y real) RETURNS real AS
 $BODY$
 BEGIN
-  RETURN (SELECT heading FROM segment, st_point(x, y) AS POINT, 
+  RETURN (SELECT heading * 180 / PI() FROM segment, st_point(x, y) AS POINT, 
                                        st_distance(st_makeline(startPoint, endPoint), point) as dis
             ORDER BY dis ASC
             LIMIT 1);
@@ -14,7 +14,7 @@ DROP FUNCTION IF EXISTS roadDirection(geometry);
 CREATE OR REPLACE FUNCTION roadDirection(cordPoint geometry) RETURNS real AS
 $BODY$
 BEGIN
-  RETURN roadDirection(ST_X(ST_Centroid(cordPoint)), ST_Y(ST_Centroid(cordPoint)));
+  RETURN roadDirection(Cast(ST_X(ST_Centroid(cordPoint)) AS real), Cast(ST_Y(ST_Centroid(cordPoint)) AS real));
 END
 $BODY$
 LANGUAGE 'plpgsql' ;
