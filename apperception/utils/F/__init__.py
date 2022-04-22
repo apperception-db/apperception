2091745b6dbe90006c268fca463ee8942514f601
Yongming Ge <46760841+YongmingFrankGe@users.noreply.github.com>
2022-04-22 12:39:46 -0700

    Add facingRelative and roadDirection sql functions to frontend translation (#73)
    * add translation for facingRelative and road_direction, also fix queries
    
    * style: [CI] format
    
    * style: [CI] format
    
    * rename facingRelative to facing_relative to preserve the convention
    
    * style: [CI] format
    
    * fix test failures
    
    Co-authored-by: Github Actions Bot <apperception-actions-bot@users.noreply.github.com>
    Co-authored-by: Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>

--- apperception/utils/F/__init__.py
+++ apperception/utils/F/__init__.py
@@ -1,5 +1,7 @@
 from .contained import contained
 from .convert_camera import convert_camera
+from .facing_relative import facing_relative
+from .road_direction import road_direction
 from .view_angle import view_angle
 
-__all__ = ["convert_camera", "view_angle", "contained"]
+__all__ = ["convert_camera", "view_angle", "contained", "facing_relative", "road_direction"]
