c7e138f611a7576f252d283c409fc984d0dafe17
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2022-04-21 23:10:24 -0700

    lambda stuff for viewAngle() and contained() (#72)
    * added lambda interpretations for sql functions
    
    * style: [CI] format
    
    Co-authored-by: YongmingFrankGe <frankge_1012@outlook.com>
    Co-authored-by: Github Actions Bot <apperception-actions-bot@users.noreply.github.com>

--- apperception/utils/F/__init__.py
+++ apperception/utils/F/__init__.py
@@ -1,3 +1,5 @@
+from .contained import contained
 from .convert_camera import convert_camera
+from .view_angle import view_angle
 
-__all__ = ["convert_camera"]
+__all__ = ["convert_camera", "view_angle", "contained"]
