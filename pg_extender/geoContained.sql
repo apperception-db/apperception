4993a8e1c5046f4bfbc2c5de9d91eafee5ba598d
yousefh409 <57015880+yousefh409@users.noreply.github.com>
2022-04-21 21:16:25 -0700

    geoContained() and viewAngle() SQL functions (#70)
    * add function setup for contained and view angle, fix syntax in the query for fig 12
    
    * implemented geoContained() and viewAngle() SQL functions
    
    Co-authored-by: YongmingFrankGe <frankge_1012@outlook.com>

---
+++ pg_extender/geoContained.sql
@@ -0,0 +1,36 @@
+/* return true if a point is contained in any of the geometry in the array*/
+
+/* this first function should already exist in mobilitydb, plz double check**/
+DROP FUNCTION IF EXISTS contained(geometry, geometry); 
+CREATE OR REPLACE FUNCTION contained(contPoint geometry, geom geometry) RETURNS boolean AS
+$BODY$
+BEGIN
+  RETURN ST_Covers(geom, contPoint);
+END
+$BODY$
+LANGUAGE 'plpgsql' ;
+
+DROP FUNCTION IF EXISTS contained(geometry, geometry[]);
+CREATE OR REPLACE FUNCTION contained(contPoint geometry, geoms geometry[]) RETURNS boolean AS
+$BODY$
+declare geom geometry;
+BEGIN
+  FOREACH geom IN ARRAY geoms
+  LOOP
+    IF contained(contPoint, geom) THEN
+      RETURN true;
+    END IF;  
+  END LOOP;
+  RETURN false; 
+END
+$BODY$
+LANGUAGE 'plpgsql' ;
+
+DROP FUNCTION IF EXISTS contained(tgeompoint, geometry[], timestamptz);
+CREATE OR REPLACE FUNCTION contained(contPoint tgeompoint, geoms geometry[], t timestamptz) RETURNS boolean AS
+$BODY$
+BEGIN
+  RETURN contained(valueAtTimestamp(contPoint, t), geoms);
+END
+$BODY$
+LANGUAGE 'plpgsql' ;
