b9b29b504d695e6013c8a7f4b2eddc0c0b2d21ee
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2022-04-23 01:18:01 -0700

    Remove unused functions (#85)
    * remove unused functions
    
    * style: [CI] format
    
    Co-authored-by: Github Actions Bot <apperception-actions-bot@users.noreply.github.com>

--- apperception/new_world.py
+++ apperception/new_world.py
@@ -1,17 +1,12 @@
 from __future__ import annotations
 
 import datetime
-import glob
 import inspect
 import uuid
-from collections.abc import Iterable
-from os import path
 from typing import Any, Callable, Dict, List, Optional, Set, Tuple, Union
 
 import cv2
-import dill as pickle
 import numpy as np
-import yaml
 from data_types import Camera, QueryType, Trajectory
 from new_db import Database
 from pypika import Table
@@ -20,11 +15,6 @@ from scenic_util import FetchCameraTuple, transformation
 
 # matplotlib.use("Qt5Agg")
 # print("get backend", matplotlib.get_backend())
-
-# makedirs("./.apperception_cache", exist_ok=True)
-
-
-BASE_VOLUME_QUERY_TEXT = "STBOX Z(({x1}, {y1}, {z1}),({x2}, {y2}, {z2}))"
 
 
 class World:
@@ -64,37 +54,8 @@ class World:
         self._types = set() if types is None else types
         self._materialized = materialized
 
-    def road_direction(self, x, y):
-        return self.db.get_heading_from_a_point(x, y)
-
-    def select_intersection_of_interest_or_use_default(self, cam_id, default=True):
-        # camera = self.camera_nodes[cam_id]
-        # video_file = camera.video_file
-        if default:
-            x1, y1, _ = 0.01082532, 2.59647246, 0
-            x2, y2, _ = 3.01034039, 3.35985782, 2
-        else:
-            raise Exception("select_intersection_of_interest_or_use_default not supported")
-            # vs = cv2.VideoCapture(video_file)
-            # frame = vs.read()
-            # frame = frame[1]
-            # cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
-            # cv2.resizeWindow("Frame", 384, 216)
-            # initBB = cv2.selectROI("Frame", frame, fromCenter=False)
-            # print(initBB)
-            # cv2.destroyAllWindows()
-            # print("world coordinate #1")
-            # tl = camera.lens.pixel_to_world(initBB[:2], 1)
-            # print(tl)
-            # x1, y1, z1 = tl
-            # print("world coordinate #2")
-            # br = camera.lens.pixel_to_world((initBB[0] + initBB[2], initBB[1] + initBB[3]), 1)
-            # print(br)
-            # x2, y2, z2 = br
-        return BASE_VOLUME_QUERY_TEXT.format(x1=x1, y1=y1, z1=0, x2=x2, y2=y2, z2=2)
-
     def overlay_trajectory(self, scene_name: str, trajectory, object_id: str):
-        frame_timestamps = self.trajectory_to_timestamp(trajectory)
+        frame_timestamps = trajectory_to_timestamp(trajectory)
         # camera_info is a list of mappings from timestamps to list of (frame_num, cameras)
         camera_info: List[Dict[int, List["FetchCameraTuple"]]] = []
         for index, cur_frame_timestamp in enumerate(frame_timestamps):
@@ -105,15 +66,9 @@ class World:
                     camera_info[index][x[6]].append(x)
                 else:
                     camera_info[index][x[6]] = [x]
-        camera_info_2 = [
-            [x[y] for y in sorted(x)] for x in camera_info
-        ]  # [x.values() for x in sorted(camera_info, key=lambda x: x[6])]
+        camera_info_2 = [[x[y] for y in sorted(x)] for x in camera_info]
 
-        # assert len(camera_info) == len(frame_num)
-        # assert len(camera_info[0]) == len(frame_num[0])
-        # print(camera_info, np.asarray(camera_info).shape) # (1, 30, 8)
-        # print(trajectory, np.asarray(trajectory).shape) # (1, 1)
-        overlay_info = self.get_overlay_info(trajectory, camera_info_2)
+        overlay_info = get_overlay_info(trajectory, camera_info_2)
         # TODO: fix the following to overlay the 2d point onto the frame
         results: List[Dict[str, List[Tuple[np.ndarray, int, str]]]] = []
         frame_width = None
@@ -150,58 +105,6 @@ class World:
                     )
                     vid_writer.write(frame_im)
                 vid_writer.release()
-
-    def trajectory_to_timestamp(self, trajectory):
-        """
-        fetch the frame number from the trajectory
-        1. get the time stamp field from the trajectory
-        2. convert the time stamp to frame number
-            Refer to 'convert_datetime_to_frame_num' in 'video_util.py'
-        3. return the frame number
-        """
-        return [traj[0][0]["datetimes"] for traj in trajectory]
-
-    def get_overlay_info(self, trajectory, camera_info: List[List[List["FetchCameraTuple"]]]):
-        """
-        overlay each trajectory 3d coordinate on to the frame specified by the camera_info
-        1. for each trajectory, get the 3d coordinate
-        2. get the camera_info associated to it
-        3. implement the transformation function from 3d to 2d
-            given the single centroid point and camera configuration
-            refer to TODO in "senic_utils.py"
-        4. return a list of (2d coordinate, frame name/filename)
-        """
-        result: List[List[List[Tuple[np.ndarray, int, str]]]] = []
-        for traj_num in range(len(trajectory)):
-            traj_obj = trajectory[traj_num][0]  # traj_obj means the trajectory of current object
-            traj_obj_3d = traj_obj["coordinates"]  # 3d coordinate list of the object's trajectory
-            camera_info_objs = camera_info[
-                traj_num
-            ]  # camera info list corresponding the 3d coordinate
-            traj_obj_2d: List[List[Tuple[np.ndarray, int, str]]] = []  # 2d coordinate list
-            for index in range(len(camera_info_objs)):
-                cur_camera_infos = camera_info_objs[
-                    index
-                ]  # camera info of the obejct in one point of the trajectory
-                centroid_3d = np.array(traj_obj_3d[index])  # one point of the trajectory in 3d
-                # in order to fit into the function transformation, we develop a dictionary called camera_config
-                frame_traj_obj_2d: List[Tuple[np.ndarray, int, str]] = []
-                for cur_camera_info in cur_camera_infos:
-                    camera_config = {}
-                    camera_config["egoTranslation"] = cur_camera_info[1]
-                    camera_config["egoRotation"] = np.array(cur_camera_info[2])
-                    camera_config["cameraTranslation"] = cur_camera_info[3]
-                    camera_config["cameraRotation"] = np.array(cur_camera_info[4])
-                    camera_config["cameraIntrinsic"] = np.array(cur_camera_info[5])
-                    traj_2d = transformation(
-                        centroid_3d, camera_config
-                    )  # one point of the trajectory in 2d
-                    framenum = cur_camera_info[6]
-                    filename = cur_camera_info[7]
-                    frame_traj_obj_2d.append((traj_2d, framenum, filename))
-                traj_obj_2d.append(frame_traj_obj_2d)
-            result.append(traj_obj_2d)
-        return result
 
     def recognize(self, camera: Camera, annotation):
         node1 = self._insert_bbox_traj(camera=camera, annotation=annotation)
@@ -282,45 +185,6 @@ class World:
             end=str(end),
         )._execute_from_root(QueryType.TRAJ)
 
-    def filter_traj_type(self, object_type: str):
-        return derive_world(
-            self, {QueryType.TRAJ}, self.db.filter_traj_type, object_type=object_type
-        )
-
-    def filter_traj_volume(self, volume: str):
-        return derive_world(self, {QueryType.TRAJ}, self.db.filter_traj_volume, volume=volume)
-
-    def filter_traj_heading(self, lessThan=float("inf"), greaterThan=float("-inf")):
-        return derive_world(
-            self,
-            {QueryType.TRAJ},
-            self.db.filter_traj_heading,
-            lessThan=lessThan,
-            greaterThan=greaterThan,
-        )
-
-    def filter_distance_to_type(self, distance: float, type: str):
-        return derive_world(
-            self, {QueryType.TRAJ}, self.db.filter_distance_to_type, distance=distance, type=type
-        )
-
-    def filter_relative_to_type(
-        self,
-        x_range: Tuple[float, float],
-        y_range: Tuple[float, float],
-        z_range: Tuple[float, float],
-        type: str,
-    ):
-        return derive_world(
-            self,
-            {QueryType.TRAJ},
-            self.db.filter_relative_to_type,
-            x_range=x_range,
-            y_range=y_range,
-            z_range=z_range,
-            type=type,
-        )
-
     def add_camera(self, camera: Camera):
         """
         1. For update method, we create two nodes: the first node will write to the db, and the second node will retrieve from the db
@@ -330,15 +194,6 @@ class World:
         node1 = self._insert_camera(camera=camera)
         node2 = node1._retrieve_camera(camera_id=camera.id)
         return node2
-
-    def interval(self, start: datetime.datetime, end: datetime.datetime):
-        return derive_world(
-            self,
-            {QueryType.BBOX},
-            self.db.interval,
-            start=str(start),
-            end=str(end),
-        )
 
     def filter(self, predicate: Union[str, Callable]):
         return derive_world(
@@ -441,7 +296,6 @@ class World:
                 if not node.done:
                     node._execute()
                     node._done = True
-                    node._update_log_file()
             # if different type => pass
             elif _type not in node.types:
                 continue
@@ -474,10 +328,6 @@ class World:
 
     def __str__(self):
         return f"fn={self._fn[0]}\nkwargs={self._kwargs}\ndone={self._done}\nworld_id={self._world_id}\n"
-
-    @property
-    def filename(self):
-        return filename(self._timestamp, self._world_id, self._name)
 
     @property
     def world_id(self):
@@ -515,83 +365,16 @@ class World:
     def materialized(self):
         return self._materialized
 
-    def _update_log_file(self):
-        # with open(self.filename, "r") as f:
-        #     children = yaml.safe_load(f).get("children_filenames", None)
-        # with open(self.filename, "w") as f:
-        #     f.write(
-        #         yaml.safe_dump(
-        #             {
-        #                 **({} if self._parent is None else {"parent": self._parent.filename}),
-        #                 **({} if self._types == set() else {"types": set(map(int, self._types))}),
-        #                 **({} if self._fn is None else {"fn": self._fn.__name__}),
-        #                 **({} if self._kwargs == {} else {"kwargs": pickle.dumps(self._kwargs)}),
-        #                 **({} if not self._done else {"done": self._done}),
-        #                 **({} if not self._materialized else {"materialized": self._materialized}),
-        #                 **({} if children is None else {"children_filenames": children}),
-        #             }
-        #         )
-        #     )
-        pass
-
 
 def empty_world(name: str) -> World:
-    matched_files = list(
-        filter(path.isfile, glob.glob(f"./.apperception_cache/*_*_{name}.ap.yaml"))
-    )
-    if len(matched_files):
-        return _empty_world_from_file(matched_files[0])
-    return _empty_world(name)
-
-
-def _empty_world_from_file(log_file: str) -> World:
-    with open(log_file, "r") as f:
-        content = yaml.safe_load(f)
-        if "children_filenames" in content:
-            del content["children_filenames"]
-        return World(*split_filename(log_file), **content)
-
-
-def _empty_world(name: str) -> World:
     world_id = str(uuid.uuid4())
     timestamp = datetime.datetime.utcnow()
-    # log_file = filename(timestamp, world_id, name)
-    # with open(log_file, "w") as f:
-    #     f.write(yaml.safe_dump({}))
     return World(world_id, timestamp, name)
 
 
 def derive_world(parent: World, types: set[QueryType], fn: Any, **kwargs) -> World:
-    # TODO: paramiterize fn's arguments and kwargs
-    # world = _derive_world_from_file(parent, types, fn, **kwargs)
-    # if world is not None:
-    #     return world
-    return _derive_world(parent, types, fn, **kwargs)
-
-
-def _derive_world(parent: World, types: set[QueryType], fn: Any, **kwargs) -> World:
     world_id = str(uuid.uuid4())
     timestamp = datetime.datetime.utcnow()
-    # log_file = filename(timestamp, world_id)
-
-    # with open(parent.filename, "r") as pf:
-    #     content = yaml.safe_load(pf)
-    # with open(parent.filename, "w") as pf:
-    #     content["children_filenames"] = content.get("children_filenames", set())
-    #     content["children_filenames"].add(log_file)
-    #     pf.write(yaml.safe_dump(content))
-
-    # with open(log_file, "w") as f:
-    #     f.write(
-    #         yaml.safe_dump(
-    #             {
-    #                 "fn": fn.__name__,
-    #                 "kwargs": pickle.dumps(kwargs),
-    #                 "parent": parent.filename,
-    #                 "types": set(map(int, types)),
-    #             }
-    #         )
-    #     )
 
     return World(
         world_id,
@@ -603,93 +386,46 @@ def _derive_world(parent: World, types: set[QueryType], fn: Any, **kwargs) -> Wo
     )
 
 
-def _derive_world_from_file(
-    parent: World, types: set[QueryType], fn: Any, **kwargs
-) -> Optional[World]:
-    with open(parent.filename, "r") as f:
-        sibling_filenames: Iterable[str] = yaml.safe_load(f).get("children_filenames", [])
-
-    for sibling_filename in sibling_filenames:
-        with open(sibling_filename, "r") as sf:
-            sibling_content = yaml.safe_load(sf)
-
-        if op_matched(sibling_content, types, fn, kwargs):
-            return World(
-                *split_filename(sibling_filename),
-                parent=parent,
-                **format_content(sibling_content),
-            )
-
-    return None
+def trajectory_to_timestamp(trajectory):
+    return [traj[0][0]["datetimes"] for traj in trajectory]
 
 
-def from_file(filename: str) -> World:
-    with open(filename, "r") as f:
-        content = yaml.safe_load(f)
-
-    parent_filename = content.get("parent_filename", None)
-    if parent_filename is None:
-        parent = None
-    else:
-        parent = from_file(parent_filename)
-
-    return World(*split_filename(filename), parent=parent, **format_content(content))
-
-
-def filename(timestamp: datetime.datetime, world_id: str, name: str = ""):
-    return f".apperception_cache/{str(timestamp).replace(':', ';')}_{world_id}_{name}.ap.yaml"
-
-
-def split_filename(filename: str) -> Tuple[str, datetime.datetime, str]:
-    filename = filename.replace("\\", "/")
-    timestamp_str, world_id, name = filename[: -len(".ap.yaml")].split("/")[-1].split("_", 2)
-    return world_id, datetime.datetime.fromisoformat(timestamp_str.replace(";", ":")), name
-
-
-DUMPED_EMPTY_DICT = pickle.dumps({})
-
-
-def double_equal(a: Tuple[Any, Any]):
-    return a[0] == a[1]
-
-
-def op_matched(
-    file_content: dict[str, Any],
-    types: set[QueryType],
-    fn: Any,
-    kwargs: Optional[dict[str, Any]] = None,
-) -> bool:
-    f_fn: str | None = file_content.get("fn", None)
-    f_types: set[int] = file_content.get("types", set())
-
-    if f_fn != fn.__name__ or f_types != set(map(int, types)):
-        return False
-
-    kwargs = {} if kwargs is None else kwargs
-    f_kwargs: dict[str, Any] = pickle.loads(file_content.get("kwargs", DUMPED_EMPTY_DICT))
-
-    if len(f_kwargs) != len(kwargs):
-        return False
-
-    cmps = fn.comparators if hasattr(fn, "comparators") else {}
-    a = all(
-        key in f_kwargs and cmps.get(key, double_equal)((f_kwargs[key], kwargs[key]))
-        for key in kwargs
-    )
-    return a
-
-
-def format_content(content: dict[str, Any]) -> dict[str, Any]:
-    if "types" in content:
-        content["types"] = set(map(QueryType, content["types"]))
-
-    if "kwargs" in content:
-        content["kwargs"] = pickle.loads(content["kwargs"])
-
-    if "parent" in content:
-        del content["parent"]
-
-    if "children_filenames" in content:
-        del content["children_filenames"]
-
-    return content
+def get_overlay_info(trajectory, camera_info: List[List[List["FetchCameraTuple"]]]):
+    """
+    overlay each trajectory 3d coordinate on to the frame specified by the camera_info
+    1. for each trajectory, get the 3d coordinate
+    2. get the camera_info associated to it
+    3. implement the transformation function from 3d to 2d
+        given the single centroid point and camera configuration
+        refer to TODO in "senic_utils.py"
+    4. return a list of (2d coordinate, frame name/filename)
+    """
+    result: List[List[List[Tuple[np.ndarray, int, str]]]] = []
+    for traj_num in range(len(trajectory)):
+        traj_obj = trajectory[traj_num][0]  # traj_obj means the trajectory of current object
+        traj_obj_3d = traj_obj["coordinates"]  # 3d coordinate list of the object's trajectory
+        camera_info_objs = camera_info[traj_num]  # camera info list corresponding the 3d coordinate
+        traj_obj_2d: List[List[Tuple[np.ndarray, int, str]]] = []  # 2d coordinate list
+        for index in range(len(camera_info_objs)):
+            cur_camera_infos = camera_info_objs[
+                index
+            ]  # camera info of the obejct in one point of the trajectory
+            centroid_3d = np.array(traj_obj_3d[index])  # one point of the trajectory in 3d
+            # in order to fit into the function transformation, we develop a dictionary called camera_config
+            frame_traj_obj_2d: List[Tuple[np.ndarray, int, str]] = []
+            for cur_camera_info in cur_camera_infos:
+                camera_config = {}
+                camera_config["egoTranslation"] = cur_camera_info[1]
+                camera_config["egoRotation"] = np.array(cur_camera_info[2])
+                camera_config["cameraTranslation"] = cur_camera_info[3]
+                camera_config["cameraRotation"] = np.array(cur_camera_info[4])
+                camera_config["cameraIntrinsic"] = np.array(cur_camera_info[5])
+                traj_2d = transformation(
+                    centroid_3d, camera_config
+                )  # one point of the trajectory in 2d
+                framenum = cur_camera_info[6]
+                filename = cur_camera_info[7]
+                frame_traj_obj_2d.append((traj_2d, framenum, filename))
+            traj_obj_2d.append(frame_traj_obj_2d)
+        result.append(traj_obj_2d)
+    return result
