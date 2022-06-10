f09517ee438de541c9de337f0c7a279728ad2107
Yongming Ge <46760841+YongmingFrankGe@users.noreply.github.com>
2022-06-10 01:21:19 -0700

    Fig14 investigate (#123)
    * working fig14, just need to fix the query and facingRelative
    
    * style: [CI] cleanup python notebooks
    
    * change facing relative to the opposite, fig14 still passes
    
    * style: [CI] cleanup python notebooks
    
    * cleanup: facingRelative.sql
    
    Co-authored-by: Github Actions Bot <apperception-actions-bot@users.noreply.github.com>
    Co-authored-by: Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>

--- pg_extender/facingRelative.sql
+++ pg_extender/facingRelative.sql
@@ -4,34 +4,43 @@
 \echo "facingRelative"
 \echo ""
 
-CREATE OR REPLACE FUNCTION facingRelative(object_heading real, camera_heading real) RETURNS real AS
+CREATE OR REPLACE FUNCTION facingRelative(target_heading real, viewpoint_heading real) RETURNS real AS
 $BODY$
 BEGIN
-  RETURN CAST((360 + object_heading) AS numeric) % 360 - CAST((360 + camera_heading) AS numeric) % 360;
+  RETURN (target_heading::numeric % 360 - viewpoint_heading::numeric % 360) % 360 ;
 END
 $BODY$
 LANGUAGE 'plpgsql' ;
 
-CREATE OR REPLACE FUNCTION facingRelative(object_heading real, camera_heading real, _time timestamptz) RETURNS real AS
+
+CREATE OR REPLACE FUNCTION facingRelative(target_heading real, viewpoint_heading real, _time timestamptz) RETURNS real AS
 $BODY$
 BEGIN
-  RETURN facingRelative(object_heading, camera_heading);
+  RETURN facingRelative(target_heading, viewpoint_heading);
 END
 $BODY$
 LANGUAGE 'plpgsql' ;
 
-CREATE OR REPLACE FUNCTION facingRelative(object_headings tfloat, camera_heading real, _time timestamptz) RETURNS real AS
+
+CREATE OR REPLACE FUNCTION facingRelative(target_headings tfloat, viewpoint_heading real, _time timestamptz) RETURNS real AS
 $BODY$
 BEGIN
-  RETURN facingRelative(CAST(valueAtTimestamp(object_headings, _time) AS real), camera_heading);
+  RETURN facingRelative(
+    valueAtTimestamp(target_headings, _time)::real,
+    viewpoint_heading
+  );
 END
 $BODY$
 LANGUAGE 'plpgsql' ;
 
-CREATE OR REPLACE FUNCTION facingRelative(object1_headings tfloat, object2_headings tfloat, _time timestamptz) RETURNS real AS
+
+CREATE OR REPLACE FUNCTION facingRelative(target_headings tfloat, viewpoint_headings tfloat, _time timestamptz) RETURNS real AS
 $BODY$
 BEGIN
-  RETURN facingRelative(CAST(valueAtTimestamp(object1_headings, _time) AS real), CAST(valueAtTimestamp(object2_headings, _time) AS real));
+  RETURN facingRelative(
+    valueAtTimestamp(target_headings, _time)::real,
+    valueAtTimestamp(viewpoint_headings, _time)::real
+  );
 END
 $BODY$
 LANGUAGE 'plpgsql' ;
