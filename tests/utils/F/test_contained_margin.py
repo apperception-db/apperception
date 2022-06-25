24c72aa34073254c22c221999595b2e93ae12682
yousefh409 <yousefh409@gmail.com>
2022-06-25 05:30:13 -0700

    slight fix

--- tests/utils/F/test_contained_margin.py
+++ tests/utils/F/test_contained_margin.py
@@ -4,7 +4,7 @@ from apperception.utils import fn_to_sql, F
 
 @pytest.mark.parametrize("fn, sql", [
     (lambda o, c: F.contained_margin(o.traj, c.timestamp, 0.5, o.timestamp), 
-        "contained_margin(T.trajCentroids, C.timestamp, 0.5, T.timestamp)"),
+        "containedMargin(T.trajCentroids, C.timestamp, 0.5, T.timestamp)"),
 ])
 
 def test_angle_between(fn, sql):
