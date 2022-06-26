8d802809a2c6884f6d7138dc6b784b3a55480762
Chanwut (Mick) Kittivorawong <30903997+chanwutk@users.noreply.github.com>
2022-06-26 02:31:42 +0200

    Add mobilitydb to Github Action (#136)
    * add docker ci
    
    * separate commands
    
    * get log
    
    * test
    
    * print logs
    
    * print logs
    
    * wait for mobilitidb to finish setup
    
    * fix
    
    * wait
    
    * add password
    
    * wait in command
    
    * setup mobilitydb first
    
    * setup
    
    * use service
    
    * ingest tables in github action
    
    * run python with poetry
    
    * style: [CI] format
    
    * style: [CI] cleanup python notebooks
    
    * fix circular dependency
    
    * fix typo
    
    * add database test
    
    * remove import pytest
    
    * fix sql error when receiving and empty result
    
    * style: [CI] format
    
    Co-authored-by: Github Actions Bot <apperception-actions-bot@users.noreply.github.com>
    Co-authored-by: YongmingFrankGe <frankge_1012@outlook.com>

---
+++ tests/test_database.py
@@ -0,0 +1,18 @@
+from apperception.database import database
+from apperception.utils import import_tables
+
+TABLE_NAMES = [
+    "Cameras",
+    "General_Bbox",
+    "Item_General_Trajectory",
+]
+
+
+def test_reset():
+    import_tables(database, './data')
+    for t in TABLE_NAMES:
+        assert not database.sql(f"select * from {t} limit 1").empty
+
+    database.reset()
+    for t in TABLE_NAMES:
+        assert database.sql("select * from " + t).empty
