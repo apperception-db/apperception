e97a10cac867b83cc3bf7f72ba5d1f32f5a99412
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-06-30 14:02:30 -0700

    add test

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,8 @@
-from apperception.database import database
+from apperception.database import Database
 from apperception.utils import import_tables
+import psycopg2
+import os
+import pytest
 
 TABLE_NAMES = [
     "Cameras",
@@ -9,10 +12,45 @@ TABLE_NAMES = [
 
 
 def test_reset():
-    import_tables(database, './data')
-    for t in TABLE_NAMES:
-        assert not database.sql(f"select * from {t} limit 1").empty
+    d = Database(psycopg2.connect(
+        dbname="mobilitydb",
+        user="docker",
+        host="localhost",
+        port=os.environ["AP_PORT_RESET"],
+        password="docker",
+    ))
 
-    database.reset()
+    import_tables(d, './data')
     for t in TABLE_NAMES:
-        assert database.sql("select * from " + t).empty
+        assert not d.sql(f"select * from {t} limit 1").empty
+
+    d.reset()
+    for t in TABLE_NAMES:
+        assert d.sql("select * from " + t).empty
+
+
+def test_execute_update_and_query():
+    d = Database(psycopg2.connect(
+        dbname="mobilitydb",
+        user="docker",
+        host="localhost",
+        port=os.environ["AP_PORT_SQL"],
+        password="docker",
+    ))
+
+    d._execute_update("create table if not exists t1 (c1 text, c2 int)")
+    d._execute_update("insert into table t1 values ('test1', 3), ('test2', 4)")
+    results = d._execute_query("select * from t1");
+    assert results == [("test1", 3), ("test2", 4)], "should return correct tuples"
+
+    with pytest.raises(psycopg2.errors.DatabaseError) as e_info:
+        d._execute_update("zxcvasdfqwer")
+
+    results = d._execute_query("select * from t1");
+    assert results == [("test1", 3), ("test2", 4)], "should execute another query after failed executions"
+
+    with pytest.raises(psycopg2.errors.DatabaseError) as e_info:
+        d._execute_query("zxcvasdfqwer")
+
+    results = d._execute_query("select * from t1");
+    assert results == [("test1", 3), ("test2", 4)], "should execute another query after failed executions"
