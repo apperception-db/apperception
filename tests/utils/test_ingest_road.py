eb29de5df6633af4a34c0122dfc5e179626904de
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-06-30 15:13:07 -0700

    fix syntax

--- tests/utils/test_ingest_road.py
+++ tests/utils/test_ingest_road.py
@@ -15,26 +15,21 @@ d = Database(psycopg2.connect(
 ingest_road(d, "./data/road_network_boston")
 
 
-def test_execute_update_and_query():
-    
-    assert d._execute_query("select count(*) from segmentpolygon") == [(0)]
-
-
 @pytest.mark.parametrize("table, count", [
-    ("segmentpolygon", 0)
-    ("segment", 0)
-    ("lanesection", 0)
-    ("lane", 0)
-    ("lane_lanesection", 0)
-    ("lanegroup", 0)
-    ("lanegroup_lane", 0)
-    ("opposite_lanegroup", 0)
-    ("road", 0)
-    ("road_lanegroup", 0)
-    ("road_roadsection", 0)
-    ("roadsection", 0)
-    ("roadsection_lanesection", 0)
-    ("intersection", 0)
+    ("segmentpolygon", 0),
+    ("segment", 0),
+    ("lanesection", 0),
+    ("lane", 0),
+    ("lane_lanesection", 0),
+    ("lanegroup", 0),
+    ("lanegroup_lane", 0),
+    ("opposite_lanegroup", 0),
+    ("road", 0),
+    ("road_lanegroup", 0),
+    ("road_roadsection", 0),
+    ("roadsection", 0),
+    ("roadsection_lanesection", 0),
+    ("intersection", 0),
 ])
 def test_simple_ops(table, count):
     assert d._execute_query(f"select count(*) from {table}") == [(count)]
