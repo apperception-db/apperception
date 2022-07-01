24f4fb28931b0e1d32c917589b71ea81a7569dd0
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-06-30 17:42:28 -0700

    fix

--- tests-scenic/test_fig_13.py
+++ tests-scenic/test_fig_13.py
@@ -4,22 +4,22 @@ from datetime import datetime, timezone
 
 def test_fig_13():
     world = empty_world(name='world')
-    world = world.filter("lambda obj1, obj2, cam: " +
-        "obj1.object_id != obj2.object_id and " +
-        "F.like(obj1.object_type, 'vehicle%') and " +
-        "F.like(obj2.object_type, 'vehicle%') and " +
-        "F.angle_between(F.facing_relative(cam.ego, F.road_direction(cam.ego, cam.ego)), -15, 15) and " +
-        "F.distance(cam.ego, obj1, cam.timestamp) < 50 and " +
-        "F.view_angle(obj1, cam.ego, cam.timestamp) < 70 / 2.0 and " +
-        "F.distance(cam.ego, obj2, cam.timestamp) < 50 and " +
-        "F.view_angle(obj2, cam.ego, cam.timestamp) < 70 / 2.0 and " +
+    world = world.filter(" ".join([
+        "lambda obj1, obj2, cam:",
+        "obj1.object_id != obj2.object_id and",
+        "F.like(obj1.object_type, 'vehicle%') and",
+        "F.like(obj2.object_type, 'vehicle%') and",
+        "F.angle_between(F.facing_relative(cam.ego, F.road_direction(cam.ego, cam.ego)), -15, 15) and",
+        "F.distance(cam.ego, obj1, cam.timestamp) < 50 and",
+        "F.view_angle(obj1, cam.ego, cam.timestamp) < 70 / 2.0 and",
+        "F.distance(cam.ego, obj2, cam.timestamp) < 50 and",
+        "F.view_angle(obj2, cam.ego, cam.timestamp) < 70 / 2.0 and",
         "F.contains_all('intersection', [obj1.traj, obj2.traj]@cam.timestamp) and "
-        "F.angle_between(F.facing_relative(obj1, cam.ego, cam.timestamp), 50, 135) and " +
-        "F.angle_between(F.facing_relative(obj2, cam.ego, cam.timestamp), -135, -50) and " +
-        "F.minDistance(cam.egoTranslation, F.road_segment('intersection')) < 10 and " +
-        "F.angle_between(F.facing_relative(obj1, obj2, cam.timestamp), 100, -100) and " +
-        "1 == 1;"
-    )
+        "F.angle_between(F.facing_relative(obj1, cam.ego, cam.timestamp), 50, 135) and",
+        "F.angle_between(F.facing_relative(obj2, cam.ego, cam.timestamp), -135, -50) and",
+        "F.minDistance(cam.egoTranslation, F.road_segment('intersection')) < 10 and",
+        "F.angle_between(F.facing_relative(obj1, obj2, cam.timestamp), 100, -100)",
+    ]))
 
     assert set(world.get_id_time_camId_filename(2)) == set([
         (
