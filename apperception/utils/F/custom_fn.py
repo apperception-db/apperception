6050afc8cce625e897e3c5e2e94eb1c56cf17f59
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-07-26 01:35:55 -0700

    update error

--- apperception/utils/F/custom_fn.py
+++ apperception/utils/F/custom_fn.py
@@ -7,7 +7,7 @@ def custom_fn(name: str, num_args: int = None):
     @call_node
     def fn(visitor: "GenSqlVisitor", args: "List[PredicateNode]"):
         if num_args is not None and len(args) != num_args:
-            raise Exception(f"Expecting {num_args} arguments, but received {len(args)}")
+            raise Exception(f"{name} is expecting {num_args} arguments, but received {len(args)}")
         return f"{name}({','.join(map(visitor, args))})"
 
     return fn
