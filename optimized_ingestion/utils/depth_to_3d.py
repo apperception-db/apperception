50809ae39d7d8386738d5aba7114d597f5c82875
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-09-07 21:44:58 -0700

    depth_to_3d

---
+++ optimized_ingestion/utils/depth_to_3d.py
@@ -0,0 +1,17 @@
+from math import sqrt
+from typing import Tuple
+
+import numpy.typing as npt
+import numpy as np
+
+
+def depth_to_3d(x: float, y: float, depth: float, intrinsic: npt.NDArray[np.float32]) -> Tuple[float, float, float]:
+    [[fx, _, x0], [_, fy, y0], [_, _, s]] = intrinsic
+
+    unit_x: float = (s * x - x0) / fx
+    unit_y: float = (s * y - y0) / fy
+
+    Z = depth / sqrt(1 + (unit_x ** 2) + (unit_y ** 2))
+    X = unit_x * Z
+    Y = unit_y * Z
+    return X, Y, Z
