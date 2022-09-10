d9024101940b1d539a7fe89e5eba7f792beb296e
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-09-10 14:47:33 -0700

    runs object recognition without saving files

--- optimized_ingestion/optimized_ingestion.py
+++ optimized_ingestion/optimized_ingestion.py
@@ -4,15 +4,11 @@ import numpy as np
 import math
 import cv2
 
-from torchvision import transforms
 from pyquaternion import Quaternion
 
 from depth_to_3d import depth_to_3d
 
-# os.chdir("../")
 from apperception.database import database
-from apperception.world import empty_world
-from apperception.utils import F
 import sys
 
 if './submodules' not in sys.path:
@@ -52,13 +48,17 @@ CAMERA_COLUMNS = [
 Helper Functions
 TODO: Clean Up
 """
+
+
 def convert_frame_to_map(frames):
     map_frame = dict(zip(CAMERA_COLUMNS, frames[:12]))
     return map_frame
 
+
 def transform_to_world(frame_coordinates, ego_translation, ego_rotation):
     ### TODO: get world coordinates
     return frame_coordinates
+
 
 def get_obj_trajectory(tracking_df, ego_config):
     '''
@@ -70,16 +70,16 @@ def get_obj_trajectory(tracking_df, ego_config):
     grouped_trajectory = tracking_df.groupby(by=["object_id"])
     for name, group in grouped_trajectory:
         obj_info[name] = {}
-        
+
         object_df = group[[
             'frame_idx', 'object_id', 'object_type', 'bbox_left', 'bbox_top', 'bbox_w', 'bbox_h']]
         object_df = object_df.reset_index(drop=True)
         framenums = group.frame_idx.tolist()
-        
+
         ### get ego_config for each framenum
         transformation_config = ego_config.iloc[framenums]
         transformation_config = transformation_config.reset_index(drop=True)
-        
+
         object_with_ego = pd.concat([object_df, transformation_config], axis=1)
         ### for each coordinate, transform
         obj_trajectory = []
@@ -116,13 +116,14 @@ def facing_relative(prev_traj_point, next_traj_point, current_ego_heading):
     result = ((diff_heading - current_ego_heading) % 360 + 360) % 360
     return result
 
+
 def facing_relative_check(obj_info, threshold, ego_config):
     facing_relative_filtered = {}
     for obj_id in obj_info:
         frame_idx = obj_info[obj_id]['frame_idx'].frame_idx
         trajectory = obj_info[obj_id]['trajectory']
         ego_heading = ego_config.iloc[frame_idx.tolist()].egoHeading.tolist()
-        filtered_idx = frame_idx[:len(ego_heading)-1][[facing_relative(trajectory[i], trajectory[i+1], ego_heading[i]) > threshold for i in range(len(ego_heading)-1)]]
+        filtered_idx = frame_idx[:len(ego_heading) - 1][[facing_relative(trajectory[i], trajectory[i + 1], ego_heading[i]) > threshold for i in range(len(ego_heading) - 1)]]
         facing_relative_filtered[obj_id] = filtered_idx
     return facing_relative_filtered
 
@@ -152,36 +153,32 @@ class TrackingFilter(Filter):
         pass
 
     def filter(self, frames: List[Frame], metadata: Dict[Any, Any]) -> Tuple[List[Frame], Dict[Any, Any]]:
-        import sample_frame_tracker as tracker
+        import trackers.yolov5_strongsort_osnet_tracker as tracker
         # Now the result is written to a txt file, need to fix this later
         
         camera_config_df = pd.DataFrame([x.get_tuple() for x in frames], columns=CAMERA_COLUMNS)
         ego_config = camera_config_df[['egoTranslation', 'egoRotation', 'egoHeading']]
         camera_config_df.filename = camera_config_df.filename.apply(lambda x: TEST_FILE_DIR + x)
         
-        result = tracker.run(camera_config_df.filename.tolist(), save_vid=True)
+        results = tracker.track(camera_config_df.filename.tolist())
 
         md = monodepth()
         depths = [md.eval(frame) for frame in camera_config_df.filename.tolist()]
 
-        df = pd.read_csv(
-            TEST_TRACK_FILE,
-            sep=",",
-            header=None,
-            names=[
-                "frame_idx",
-                "object_id",
-                "bbox_left",
-                "bbox_top",
-                "bbox_w",
-                "bbox_h",
-                "None1",
-                "None2",
-                "None3",
-                "None",
-                "object_type"
-            ]
-        )
+        df = pd.DataFrame(results, columns=[
+            "frame_idx",
+            "object_id",
+            "bbox_left",
+            "bbox_top",
+            "bbox_w",
+            "bbox_h",
+            "None1",
+            "None2",
+            "None3",
+            "None",
+            "object_type",
+            "conf",
+        ])
 
         intrinsics = camera_config_df['cameraIntrinsic'].tolist()
 
@@ -195,7 +192,7 @@ class TrackingFilter(Filter):
 
         df[['3d-x', '3d-y', '3d-z']] = df.apply(find_3d_location)
         df.frame_idx = df.frame_idx.add(-1)
-        
+
         obj_info = get_obj_trajectory(df, ego_config)
         facing_relative_filtered = facing_relative_check(obj_info, 0, ego_config)
         for obj_id in facing_relative_filtered:
@@ -203,7 +200,7 @@ class TrackingFilter(Filter):
             filtered_idx = facing_relative_filtered[obj_id]
             current_obj_filtered_frames = camera_config_df.filename.iloc[filtered_idx]
             current_obj_bboxes = obj_info[obj_id]['bbox']
-            
+
             # choose codec according to format needed
             fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
             video = cv2.VideoWriter('../imposed_video/' + str(obj_id) + '_video.avi', fourcc, 1, (1600, 900))
@@ -211,7 +208,7 @@ class TrackingFilter(Filter):
             for i in range(len(current_obj_filtered_frames)):
                 img = cv2.imread(current_obj_filtered_frames.iloc[i])
                 x, y, x_w, y_h = current_obj_bboxes[filtered_idx.index[i]]
-                cv2.rectangle(img,(x,y),(x_w,y_h),(0,255,0),2)
+                cv2.rectangle(img, (x, y), (x_w, y_h), (0, 255, 0), 2)
                 video.write(img)
 
             cv2.destroyAllWindows()
--- optimized_ingestion/trackers/yolov5_strongsort_osnet_tracker.py
+++ optimized_ingestion/trackers/yolov5_strongsort_osnet_tracker.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 # limit the number of cpus used by high performance libraries
 os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
@@ -7,7 +8,6 @@ os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 
 
-import glob
 import sys
 from pathlib import Path
 import torch
@@ -28,12 +28,11 @@ if str(ROOT / 'strong_sort/deep/reid') not in sys.path:
 
 import logging
 from Yolov5_StrongSORT_OSNet.yolov5.models.common import DetectMultiBackend
-from Yolov5_StrongSORT_OSNet.yolov5.utils.dataloaders import VID_FORMATS, LoadImages
+from Yolov5_StrongSORT_OSNet.yolov5.utils.dataloaders import LoadImages
 from Yolov5_StrongSORT_OSNet.yolov5.utils.general import (
-    LOGGER, check_img_size, non_max_suppression, scale_coords, cv2, xyxy2xywh, increment_path, strip_optimizer, colorstr
+    LOGGER, check_img_size, non_max_suppression, scale_coords, xyxy2xywh, increment_path
 )
 from Yolov5_StrongSORT_OSNet.yolov5.utils.torch_utils import select_device, time_sync
-from Yolov5_StrongSORT_OSNet.yolov5.utils.plots import Annotator, colors, save_one_box
 from Yolov5_StrongSORT_OSNet.strong_sort.utils.parser import get_config
 from Yolov5_StrongSORT_OSNet.strong_sort.strong_sort import StrongSORT
 
@@ -53,36 +52,25 @@ save_dir = str(APPERCEPTION / "tracks/") + "/"
 # Load model
 device = select_device('')
 half = False
-webcam = False
 model = DetectMultiBackend(yolo_weights, device=device, dnn=False, data=None, fp16=half)
 stride, names, pt = model.stride, model.names, model.pt
 imgsz = check_img_size((640, 640), s=stride)  # check image size
 
 
 @torch.no_grad()
-def run(
+def track(
     source=[],  # take in a list of frames, treat it as a video
     conf_thres=0.25,  # confidence threshold
     iou_thres=0.45,  # NMS IOU threshold
     max_det=1000,  # maximum detections per image
-    show_vid=False,  # show results
-    save_conf=False,  # save confidences in --save-txt labels
-    save_crop=False,  # save cropped prediction boxes
-    save_vid=False,  # save confidences in --save-txt labels
-    nosave=False,  # do not save images/videos
     classes=None,  # filter by class: --class 0, or --class 0 2 3
     agnostic_nms=False,  # class-agnostic NMS
     augment=False,  # augmented inference
     visualize=False,  # visualize features
-    update=False,  # update all models
-    line_thickness=3,  # bounding box thickness (pixels)
-    hide_labels=False,  # hide labels
-    hide_conf=False,  # hide confidences
-    hide_class=False,  # hide IDs
 ):
     dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
     nr_sources = 1
-    vid_path, vid_writer, txt_path = [None] * nr_sources, [None] * nr_sources, [None] * nr_sources
+    labels: List[tuple] = []
 
     # initialize StrongSORT
     cfg = get_config()
@@ -136,30 +124,12 @@ def run(
         # Process detections
         for i, det in enumerate(pred):  # detections per image
             seen += 1
-            if webcam:  # nr_sources >= 1
-                p, im0, _ = path[i], im0s[i].copy(), dataset.count
-                p = Path(p)  # to Path
-                s += f'{i}: '
-                txt_file_name = p.name
-                save_path = save_dir + p.name  # im.jpg, vid.mp4, ...
-            else:
-                p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
-                p = Path(p)  # to Path
-                # video file
-                if isinstance(source, str) and source.endswith(VID_FORMATS):
-                    txt_file_name = p.stem
-                    save_path = save_dir + p.name  # im.jpg, vid.mp4, ...
-                # folder with imgs
-                else:
-                    txt_file_name = p.parent.name  # get folder name containing current img
-                    save_path = save_dir + p.parent.name  # im.jpg, vid.mp4, ...
+            p, im0, _ = path, im0s.copy(), getattr(dataset, 'frame', 0)
+            p = Path(p)  # to Path
             curr_frames[i] = im0
 
-            txt_path = save_dir + txt_file_name  # im.txt
             s += '%gx%g ' % im.shape[2:]  # print string
-            imc = im0.copy() if save_crop else im0  # for save_crop
 
-            annotator = Annotator(im0, line_width=2, pil=not ascii)
             if cfg.STRONGSORT.ECC:  # camera motion compensation
                 strongsort_list[i].tracker.camera_update(prev_frames[i], curr_frames[i])
 
@@ -186,29 +156,18 @@ def run(
                 if len(outputs[i]) > 0:
                     for j, (output, conf) in enumerate(zip(outputs[i], confs)):
 
-                        bboxes = output[0:4]
                         id = output[4]
                         cls = output[5]
                         c = int(cls)
-                        if save_txt:
-                            # to MOT format
-                            bbox_left = output[0]
-                            bbox_top = output[1]
-                            bbox_w = output[2] - output[0]
-                            bbox_h = output[3] - output[1]
-                            # Write MOT compliant results to file
-                            with open(txt_path + '.txt', 'a') as f:
-                                # MOT format
-                                f.write(('%g,' * 10 + f'{names[c]}\n') % (frame_idx, id, bbox_left, bbox_top, bbox_w, bbox_h, -1, -1, -1, i))
 
-                        if save_vid or save_crop or show_vid:  # Add bbox to image
-                            c = int(cls)  # integer class
-                            id = int(id)  # integer id
-                            label = None if hide_labels else (f'{id} {names[c]}' if hide_conf else (f'{id} {conf:.2f}' if hide_class else f'{id} {names[c]} {conf:.2f}'))
-                            annotator.box_label(bboxes, label, color=colors(c, True))
-                            if save_crop:
-                                txt_file_name = txt_file_name if (isinstance(path, list) and len(path) > 1) else ''
-                                save_one_box(bboxes, imc, file=save_dir / 'crops' / txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)
+                        # to MOT format
+                        bbox_left = output[0]
+                        bbox_top = output[1]
+                        bbox_w = output[2] - output[0]
+                        bbox_h = output[3] - output[1]
+                        labels.append(
+                            (frame_idx, id, bbox_left, bbox_top, bbox_w, bbox_h, -1, -1, -1, i, f"{names[c]}", conf)
+                        )
 
                 LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s), StrongSORT:({t5 - t4:.3f}s)')
 
@@ -216,35 +175,9 @@ def run(
                 strongsort_list[i].increment_ages()
                 LOGGER.info('No detections')
 
-            # Stream results
-            im0 = annotator.result()
-            if show_vid:
-                cv2.imshow(str(p), im0)
-                cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_vid:
-                if vid_path[i] != save_path:  # new video
-                    vid_path[i] = save_path
-                    if isinstance(vid_writer[i], cv2.VideoWriter):
-                        vid_writer[i].release()  # release previous video writer
-                    if vid_cap:  # video
-                        fps = vid_cap.get(cv2.CAP_PROP_FPS)
-                        w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-                        h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                    else:  # stream
-                        fps, w, h = 30, im0.shape[1], im0.shape[0]
-                    save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
-                    vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
-                vid_writer[i].write(im0)
-
             prev_frames[i] = curr_frames[i]
 
     # Print results
     t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
     LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, *imgsz)}' % t)
-    if save_txt or save_vid:
-        s = f"\n{len(list(glob.glob(save_dir + '/*.txt')))} tracks saved to {save_dir}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
-    if update:
-        strip_optimizer(yolo_weights)  # update model (to fix SourceChangeWarning)
+    return labels
