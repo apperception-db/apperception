07237dd69f5759374c9389d4ee0034aa75996871
yousefh409 <yousefh409@gmail.com>
2022-09-20 23:26:19 -0700

    detects if any part of the intersection is in view

--- pg_extender/in_view.sql
+++ pg_extender/in_view.sql
@@ -2,16 +2,32 @@
 DROP FUNCTION IF EXISTS inView(text, real, geometry, real, real);
 CREATE OR REPLACE FUNCTION inView(segment_type text, view_point_heading real, view_point geometry, max_distance real, view_angle real) RETURNS boolean AS 
 $BODY$
-declare clockwise numeric;
-declare counterClockwise numeric;
-declare azimuth numeric;
+declare leftViewLine geometry;
+declare rightViewLine geometry;
 -- Note: view_point_heading is counter-clockwise with North being 0, while the result of ST_Azimuth is clockwise with North being 0
 BEGIN
-
+    leftViewLine := viewAngleLine(view_point_heading, view_point, max_distance, -view_angle);
+    rightViewLine := viewAngleLine(view_point_heading, view_point, max_distance, view_angle);
     RETURN EXISTS(SELECT * FROM SegmentPolygon 
                         WHERE segment_type = Any(segmentTypes) 
                           AND ST_Distance(view_point, elementPolygon) < max_distance 
-                          AND viewAngle(ST_Centroid(elementPolygon) , view_point_heading, view_point) < view_angle);
+                          AND (viewAngle(ST_Centroid(elementPolygon) , view_point_heading, view_point) < view_angle
+                               OR ST_Intersects(elementPolygon, leftViewLine)
+                               OR ST_Intersects(elementPolygon, rightViewLine)
+                              )
+                  );
+END
+$BODY$
+LANGUAGE 'plpgsql' ;
+
+
+DROP FUNCTION IF EXISTS viewAngleLine(real, geometry, real, real);
+CREATE OR REPLACE FUNCTION viewAngleLine(view_point_heading real, view_point geometry, max_distance real, view_angle real) RETURNS geometry AS
+$BODY$
+BEGIN 
+  RETURN ST_Translate(ST_Rotate(ST_MakeLine(ST_MakePoint(0, 0), ST_MakePoint(0, max_distance)),
+                                            radians(view_point_heading + view_angle)), 
+                                            ST_X(view_point), ST_Y(view_point));
 END
 $BODY$
 LANGUAGE 'plpgsql' ;
