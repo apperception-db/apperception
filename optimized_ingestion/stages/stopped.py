7a6d253683a9884f75deaf9f412b279db8c8ef59
yousefh409 <yousefh409@gmail.com>
2022-09-22 00:33:24 -0700

    stopped filter

---
+++ optimized_ingestion/stages/stopped.py
@@ -0,0 +1,52 @@
+from typing import TYPE_CHECKING, Optional, Tuple
+
+from bitarray import bitarray
+
+from apperception.database import database
+
+from .stage import Stage
+
+if TYPE_CHECKING:
+    from ..payload import Payload
+
+
+"""
+Filters all frames where the ego is stopped, since this will mean that there is a red light and as 
+such, there won't be cars going in the intersection in the same direction (assuming traffic rules)
+"""
+class Stopped(Stage):
+    def __init__(self, min_stopped_frames: int, stopped_threshold: float) -> None:
+        self.min_stopped_frames = min_stopped_frames
+        self.stopped_threshold = stopped_threshold
+
+    def __call__(self, payload: "Payload") -> "Tuple[Optional[bitarray], Optional[list]]":
+        keep = bitarray()
+        frames = list(payload.video)
+        stopped = set()
+        for i in range(1, len(frames)):
+            _frame = frames[i]
+            _prev_frame = frames[i - 1]
+
+            current_point = f"'POINT ({' '.join([*map(str, _frame.ego_translation)])})'"
+            prev_point = f"'POINT ({' '.join([*map(str, _prev_frame.ego_translation)])})'"
+
+            query = (
+                f"SELECT ABS(ST_Distance({current_point}, {prev_point}))"
+            )
+            result = database._execute_query(query)[0][0]
+            
+            if result <= self.stopped_threshold:
+                stopped.add(i)
+                stopped.add(i - 1)
+
+        for i in len(frames):
+            min_stopped = set()
+            for j in range(i - self.min_stopped_frames, i + self.min_stopped_frames + 1):
+                min_stopped.add(j)
+            
+            if min_stopped.issubset(stopped):
+                keep.append(True)
+            else:
+                keep.append(False)
+
+        return keep, None
