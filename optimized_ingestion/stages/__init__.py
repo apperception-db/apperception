db6720093e5fcbf7d8158fd5ea8b56358c57900f
yousefh409 <yousefh409@gmail.com>
2022-09-22 13:47:54 -0700

    updated main

--- optimized_ingestion/stages/__init__.py
+++ optimized_ingestion/stages/__init__.py
@@ -2,10 +2,12 @@ from .in_view import InView
 from .sample import Sample
 from .stage import Stage
 from .tracking_filter import TrackingFilter
+from .stopped import Stopped
 
 __all__ = [
     "Stage",
     "Sample",
     "InView",
     "TrackingFilter",
+    "Stopped",
 ]
