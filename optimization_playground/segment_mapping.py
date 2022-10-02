8c35aa647dd413d574263f4fbfe4fc01bd8a1414
YongmingFrankGe <frankge_1012@outlook.com>
2022-10-02 16:45:26 -0700

    add typing

--- optimization_playground/segment_mapping.py
+++ optimization_playground/segment_mapping.py
@@ -4,6 +4,8 @@ Input: a video, camera config, road network
 Output: estimation of object trajectory
 """
 
+from typing import Any, Dict, Tuple, List, Set, Union
+
 from collections import namedtuple
 import os
 import math
@@ -60,15 +62,23 @@ SEGMENT_DWITHIN_QUERY = """select * from segmentpolygon
 cam_segment_mapping = namedtuple('cam_segment_mapping', ['cam_segment', 'road_segment_info'])
 
 class roadSegmentInfo:
-    def __init__(self, segment_id, segment_polygon, segment_type, contains_ego, ego_config, fov_lines):
+    def __init__(
+        self, 
+        segment_id: int,
+        segment_polygon: Tuple[Tuple[float, float]],
+        segment_type: str,
+        contains_ego: bool,
+        ego_config: Dict[str, Any],
+        fov_lines: Tuple[Tuple[Tuple[float, float], Tuple[float, float]],
+                         Tuple[Tuple[float, float], Tuple[float, float]]]):
         """
-        segment_id: string
-        segment_polygon: tuple of sympy.Point
-        segment_type: string
-        contains_ego: boolean
-        ego_config: dict
+        segment_id: unique segment id
+        segment_polygon: tuple of (x, y) coordinates
+        segment_type: road segment type
+        contains_ego: whether the segment contains ego camera
+        ego_config: ego camfig for the frame we asks info for
         facing_relative: float
-        fov_lines: tuple(tuple(Point, Point), tuple(Point, Point))
+        fov_lines: field of view lines
         """
         self.segment_id = segment_id
         self.segment_polygon = segment_polygon
@@ -78,30 +88,35 @@ class roadSegmentInfo:
         self.facing_relative = self.facing_relative(ego_config['egoHeading'], segment_id)
         self.fov_lines = fov_lines
     
-    def facing_relative(self, ego_heading, segment_id):
+    def facing_relative(self, ego_heading: float, segment_id: str) -> float:
         return
 
-def road_segment_contains(ego_config):
+def road_segment_contains(ego_config: Dict[str, Any])\
+        -> Tuple[Tuple[str, str, set]]:
     query = SEGMENT_CONTAIN_QUERY.format(
         ego_translation=Point(*ego_config['egoTranslation']).wkb_hex)
 
     return database._execute_query(query)
 
-def find_segment_dwithin(start_segment, view_distance=50):
+def find_segment_dwithin(start_segment: str,
+                         view_distance=50) -> Tuple[Tuple[str, str, set]]:
     start_segment_id, start_segment_polygon, segmenttype, contains_ego = start_segment
     query = SEGMENT_DWITHIN_QUERY.format(
         start_segment=start_segment_polygon, view_distance=view_distance)
 
     return database._execute_query(query)
 
-def reformat_return_segment(segments):
-    return list(map(lambda x: (x[0], x[1], tuple(x[2]) if x[2] is not None else None), segments))
+def reformat_return_segment(segments: Tuple[str, str, set])\
+        -> List[Tuple[str, str, Tuple[str]]]:
+    return list(map(
+        lambda x: (x[0], x[1], tuple(x[2]) if x[2] is not None else None), segments))
 
-def annotate_contain(segments, contain=False):
+def annotate_contain(segments: tuple, contain=False):
     for i in range(len(segments)):
         segments[i] = segments[i] + (contain,)
 
-def construct_search_space(ego_config, view_distance=50):
+def construct_search_space(ego_config: Dict[str, Any],
+                           view_distance=50) -> Set[Tuple[str, str, Tuple[str]]]:
     '''
     road segment: (elementid, elementpolygon, segmenttype, contains_ego?)
     view_distance: in meters, default 50 because scenic standard
@@ -118,7 +133,9 @@ def construct_search_space(ego_config, view_distance=50):
     search_space.update(segment_within_distance)
     return search_space
 
-def get_fov_lines(ego_config, ego_fov=70):
+def get_fov_lines(ego_config: Dict[str, Any],
+                  ego_fov=70) -> Tuple[Tuple[Tuple[float, float], Tuple[float, float]],
+                                       Tuple[Tuple[float, float], Tuple[float, float]]]:
     '''
     return: two lines representing fov in world coord
     '''
@@ -134,7 +151,9 @@ def get_fov_lines(ego_config, ego_fov=70):
          y_ego + math.sin(right_degree)*50))
     return left_fov_line, right_fov_line
 
-def intersection(fov_line, segmentpolygon):
+def intersection(fov_line: Tuple[Tuple[Tuple[float, float], Tuple[float, float]],
+                                 Tuple[Tuple[float, float], Tuple[float, float]]],
+                 segmentpolygon: Polygon) -> Tuple[Tuple[float, float]]:
     '''
     return: intersection point: tuple[tuple]
     '''
@@ -143,11 +162,15 @@ def intersection(fov_line, segmentpolygon):
     right_intersection = tuple(LineString(right_fov_line).intersection(segmentpolygon).coords)
     return left_intersection + right_intersection
 
-def in_frame(transformed_point, frame_size):
+def in_frame(transformed_point: np.array, frame_size: Tuple[int, int]):
     return transformed_point[0] > 0 and transformed_point[0] < frame_size[0] and \
         transformed_point[1] < frame_size[1] and transformed_point[1] > 0
 
-def in_view(road_point, ego_translation, fov_lines):
+def in_view(
+    road_point: Tuple,
+    ego_translation: List[float],
+    fov_lines: Tuple[Tuple[Tuple[float, float], Tuple[float, float]],
+                     Tuple[Tuple[float, float], Tuple[float, float]]]) -> bool:
     '''
     return if the road_point is on the left of the left fov line and 
                                 on the right of the right fov line
@@ -160,8 +183,19 @@ def in_view(road_point, ego_translation, fov_lines):
     return (left_fov_line_x - Ax) * (My - Ay) - (left_fov_line_y - Ay) * (Mx - Ax) <= 0 and \
               (right_fov_line_x - Ax) * (My - Ay) - (right_fov_line_y - Ay) * (Mx - Ax) >= 0
 
-def construct_mapping(decoded_road_segment, frame_size, fov_lines, segmentid,
-                      segmenttype, contains_ego, ego_config):
+def construct_mapping(
+    decoded_road_segment: Tuple[Tuple[float, float]],
+    frame_size: Tuple[int, int],
+    fov_lines: Tuple[Tuple[Tuple[float, float], Tuple[float, float]],
+                     Tuple[Tuple[float, float], Tuple[float, float]]],
+    segmentid: str,
+    segmenttype: str,
+    contains_ego: bool,
+    ego_config: Dict[str, Any]) -> Tuple[bool, cam_segment_mapping]:
+    """
+    Given current road segment
+    determine whether add it to the mapping
+    """
     ego_translation = ego_config['egoTranslation'][:2]
     if contains_ego:
         decoded_road_segment += (ego_translation,)
@@ -190,10 +224,10 @@ def construct_mapping(decoded_road_segment, frame_size, fov_lines, segmentid,
                 ))
             )
 
-def map_imgsegment_roadsegment(ego_config, frame_size=(1600, 900)):
+def map_imgsegment_roadsegment(ego_config: Dict[str, Any],
+                               frame_size=(1600, 900)) -> List[cam_segment_mapping]:
     '''
     FULL ALGORITHM:
-    Greedy
     road_segment_info: {segmentid,
                         segmentpolygon,
                         segment_type, 
@@ -205,11 +239,9 @@ def map_imgsegment_roadsegment(ego_config, frame_size=(1600, 900)):
     2. For road_segment in search_space:
         intersect_point = intersection(fov_lines, road_segment)
         cam_segment = filter([road_segment_world_coord, intersection_point], 
-                              lambda point: transform(point) in frame)
+                              lambda point: point in view)
         if cam_segment is valid:
             append_to_mapping({cam_segments:road_segment})
-            
-        search_space.append(find_next_segment(current_segment))
     '''
     fov_lines = get_fov_lines(ego_config)
     start_time = time.time()
@@ -240,7 +272,7 @@ def map_imgsegment_roadsegment(ego_config, frame_size=(1600, 900)):
     print('total mapping time: ', time.time() - start_time)
     return mapping
 
-def visualization(test_img_path, test_config, mapping):
+def visualization(test_img_path: str, test_config: Dict[str, Any], mapping: Tuple):
     """
     visualize the mapping from camera segment to road segment
     for testing only
