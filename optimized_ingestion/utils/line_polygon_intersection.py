34ff64b3cafaf16a33d0b6864bd18eb8b8d7e062
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-10-07 13:41:36 -0700

    .

---
+++ optimized_ingestion/utils/line_polygon_intersection.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple
+import numpy.typing as npt
+
+
+def line_polygon_intersection(
+    line: "npt.NDArray",
+    polygon: "List[Tuple[float, float, float]]"
+) -> "Tuple[float, float, float]":
+    # TODO: find 3D point
+    # TODO: offset 0.5 pixel
+    # TODO: check if the point is in the polygon
+    pass
