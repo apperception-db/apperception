a6d1eb0a032a39dee893dcfaaeeaf7182d0d2fa8
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-11-30 02:16:55 -0800

    detection estimation as stage

--- optimized_ingestion/stages/detection_estimation.py
+++ optimized_ingestion/stages/detection_estimation.py
@@ -1,7 +1,148 @@
+import time
+from bitarray import bitarray
+
+from shapely.geometry import Polygon
+import numpy.typing as npt
+
+from typing import List, Tuple
+
+from ..video import Video
+from ..camera_config import CameraConfig
+
+from ..detection_estimation.segment_mapping import map_imgsegment_roadsegment, CameraSegmentMapping
+from ..detection_estimation.utils import trajectory_3d
+from ..detection_estimation.detection_estimation import construct_all_detection_info, detection_to_img_segment, obj_detection, generate_sample_plan, DetectionInfo, samplePlan
+
+from .detection_2d.yolo_detection import YoloDetection
+from .detection_2d.detection_2d import Detection2D
+
 from ..payload import Payload
 from .stage import Stage, StageOutput
 
 
 class DetectionEstimation(Stage):
     def _run(self, payload: "Payload") -> "StageOutput":
-        return super()._run(payload)
+        if Detection2D.get(payload) is None:
+            raise Exception()
+        
+        ego_trajectory = [trajectory_3d(f.ego_translation, f.timestamp) for f in payload.video]
+        return dry_run(payload, 0, ego_trajectory)
+
+
+def generate_sample_plan_once(
+    video: "Video",
+    ego_config: "CameraConfig",
+    mapping: "List[CameraSegmentMapping]",
+    next_frame_num: "int",
+    car_loc3d=None,
+    target_car_detection=None,
+    all_detection_info: "List[DetectionInfo] | None" = None
+) -> "Tuple[samplePlan, None]":
+    # if all_detection_info is None:
+    #     assert target_car_detection and car_loc3d
+    #     x,y,w,h = list(map(int, target_car_detection))
+    #     car_loc2d = (x, y+h//2)
+    #     car_bbox2d = (x-w//2,y-h//2,x+w//2,y+h//2)
+    #     car_bbox3d = None
+    #     all_detections = []
+    #     all_detections.append(obj_detection('car_1', car_loc3d, car_loc2d, car_bbox3d, car_bbox2d))
+    #     all_detection_info = construct_all_detection_info(cam_segment_mapping, ego_trajectory, ego_config, all_detections)
+    assert all_detection_info is not None
+    if all_detection_info:
+        print(all_detection_info[0].road_type)
+    next_sample_plan = generate_sample_plan(video, next_frame_num, all_detection_info,  50)
+    # next_frame = None
+    next_sample_frame_info = next_sample_plan.get_next_sample_frame_info()
+    if next_sample_frame_info:
+        next_sample_frame_name, next_sample_frame_num, _ = next_sample_frame_info
+        print("next frame name", next_sample_frame_name)
+        print("next frame num", next_sample_frame_num)
+        # print(next_sample_plan.action)
+        # TODO: should not read next frame -> get the next frame from frames.pickle
+        # next_frame = cv2.imread(test_img_base_dir+next_sample_frame_name)
+        # cv2.imshow("next_frame", next_frame)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+    return next_sample_plan, None
+
+
+def construct_estimated_all_detection_info(
+    detections: "npt.NDArray",
+    cam_segment_mapping: "List[CameraSegmentMapping]",
+    ego_config: "CameraConfig",
+    ego_trajectory: "trajectory_3d"
+) -> "List[DetectionInfo]":
+    all_detections = []
+    for det in detections:
+        bbox = det[:4]
+        obj_cls = det[5]
+        x, y, x2, y2 = list(map(int,bbox))
+        w = x2 - x
+        h = y2 - y
+        car_loc2d = (x + w // 2, y+h//2)
+        # print(car_loc2d)
+        car_bbox2d = ((x-w//2, y-h//2), (x+w//2, y+h//2))
+        car_bbox3d = None
+        estimate_3d = detection_to_img_segment(car_loc2d, cam_segment_mapping)
+        if estimate_3d and estimate_3d.road_segment_info.segment_type in ['lane', 'laneSection']:
+            car_loc3d = tuple(Polygon(estimate_3d.road_segment_info.segment_polygon).centroid.coords)
+            # print(tuple(car_loc3d))
+            all_detections.append(obj_detection('car_1', car_loc3d, car_loc2d, car_bbox3d, car_bbox2d))
+    print("all_detections", all_detections)
+    all_detection_info = construct_all_detection_info(cam_segment_mapping, ego_config, ego_trajectory, all_detections)
+    return all_detection_info
+
+
+def dry_run(
+    payload: "Payload",
+    start_frame_num: "int",
+    ego_trajectory: "List[trajectory_3d]",
+) -> "Tuple[bitarray, None]":
+    skipped_frame_num = []
+    next_frame_num = start_frame_num
+    action_type_counts = {}
+    # start_time = time.time()
+    total_detection_time = 0
+    total_sample_plan_time = 0
+    for i in range(len(payload.video)-1):
+        current_ego_config = payload.video[i]
+        if i != next_frame_num:
+            skipped_frame_num.append(i)
+            continue
+        next_frame_num = i + 1
+        cam_segment_mapping = map_imgsegment_roadsegment(current_ego_config)
+        # print("mapping length", len(cam_segment_mapping))
+        start_detection_time = time.time()
+        dets = YoloDetection.get(payload)
+        assert dets is not None
+        det = dets[i]
+        all_detection_info = construct_estimated_all_detection_info(det[0], cam_segment_mapping, current_ego_config, ego_trajectory[i])
+        total_detection_time += time.time()-start_detection_time
+        start_generate_sample_plan = time.time()
+        next_sample_plan, _ = generate_sample_plan_once(payload.video, current_ego_config, cam_segment_mapping, next_frame_num, all_detection_info=all_detection_info)
+        total_sample_plan_time += time.time() - start_generate_sample_plan
+        next_action_type = next_sample_plan.get_action_type()
+        if next_action_type not in action_type_counts:
+            action_type_counts[next_action_type] = 1
+        else:
+            action_type_counts[next_action_type] += 1
+        next_frame_num = next_sample_plan.get_next_frame_num(next_frame_num)
+    # print("sorted_ego_config_length", len(payload.video))
+    # print("number of skipped", len(skipped_frame_num))
+    # print(skipped_frame_num)
+    # print(action_type_counts)
+    # total_run_time = time.time()-start_time
+    # num_runs = len(payload.video) - len(skipped_frame_num)
+    # print("total_run_time", total_run_time)
+    # print("avg run time", total_run_time/num_runs)
+    # print("total_detection_time", total_detection_time)
+    # print("avg detection time", total_detection_time/num_runs)
+    # print("total_generate_sample_plan_time", total_sample_plan_time)
+    # print("avg generate_sample_plan time", total_sample_plan_time/num_runs)
+    
+    keep = bitarray(len(payload.video))
+    keep[:] = 1
+    for f in skipped_frame_num:
+        keep[f] = 0
+    
+    return keep, None
