c07ee5aaceb23b23f7f7c60b7ce287c24bb6efee
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2022-12-14 23:28:27 -0800

    document 3d detection dimensions

--- optimized_ingestion/stages/detection_3d/__init__.py
+++ optimized_ingestion/stages/detection_3d/__init__.py
@@ -3,6 +3,26 @@ from typing import List, Tuple
 
 from ..stage import Stage
 
+
+# tensor dimension n x 18:
+#   - bbox_left
+#   - bbox_top
+#   - bbox_w
+#   - bbox_h
+#   - conf
+#   - class
+#   - bbox3d_left_x
+#   - bbox3d_left_y
+#   - bbox3d_left_z
+#   - bbox3d_right_x
+#   - bbox3d_right_y
+#   - bbox3d_right_z
+#   - bbox3d_from_camera_left_x
+#   - bbox3d_from_camera_left_y
+#   - bbox3d_from_camera_left_z
+#   - bbox3d_from_camera_right_x
+#   - bbox3d_from_camera_right_y
+#   - bbox3d_from_camera_right_z
 Metadatum = Tuple[torch.Tensor, List[str]]
 
 
