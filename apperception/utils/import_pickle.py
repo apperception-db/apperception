84445f811d6ddc1cd10f837af4125ab9eed94bfa
Apperception Service <apperception@freddie.Millennium.Berkeley.EDU>
2023-03-13 13:10:28 -0700

    add more todos

--- apperception/utils/import_pickle.py
+++ apperception/utils/import_pickle.py
@@ -12,7 +12,7 @@ def import_pickle(database: "Database", data_path: str):
     database.reset(True)
     for scene, val in data_frames.items():
         if val['location'] == 'boston-seaport':
-            cameraid = val["frames"][0][0]
+            cameraid = val["frames"][0][0] ### TODO(mick): fix cameraids for full dataset
             configs = [
                 CameraConfig(
                     frame_id=frame[1],
