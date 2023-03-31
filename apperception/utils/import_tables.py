83665b1cc32bf49f07e9e386bbea261ccca22c49
Chanwut (Mick) Kittivorawong <chanwutk@gmail.com>
2023-03-31 23:45:33 +0000

    fix import_tables for test

--- apperception/utils/import_tables.py
+++ apperception/utils/import_tables.py
@@ -16,6 +16,7 @@ def import_tables(database: "Database", data_path: str):
         os.path.join(data_path, "item_general_trajectory.csv")
     )
     df_Item_General_Trajectory = pd.DataFrame(data_Item_General_Trajectory)
+    df_Item_General_Trajectory.drop(columns=["color", "largestbbox"], inplace=True)
 
     data_General_Bbox = pd.read_csv(os.path.join(data_path, "general_bbox.csv"))
     df_General_Bbox = pd.DataFrame(data_General_Bbox)
