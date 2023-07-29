57330afc102cd38118fe2e82a44c924dd1bf2806
YongmingFrankGe <yongmg@berkeley.edu>
2023-07-29 02:37:37 +0000

    skip if exits intersection

--- optimized_ingestion/stages/detection_estimation/optimized_segment_mapping.py
+++ optimized_ingestion/stages/detection_estimation/optimized_segment_mapping.py
@@ -274,7 +274,7 @@ def get_detection_polygon_mapping(detections: "list[obj_detection]", ego_config:
         assert types is not None
         assert heading is not None
     mapped_polygons = reformat_return_polygon(mapped_polygons)
-    if any(p.type == 'intersection' for p in mapped_polygons):
+    if any(p.road_type == 'intersection' for p in mapped_polygons):
         return []
     mapped_road_polygon_info = {}
     fov_lines = get_fov_lines(ego_config)
